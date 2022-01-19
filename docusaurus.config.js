def5f8efce36ad93fab5ef593d16a1884774fe27
Hooopo Wang <hoooopo@gmail.com>
2022-01-19 18:40:08 +0800

    deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://staticsiteg.github.io/',
+  baseUrl: '/docus/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'staticsiteg', // Usually your GitHub org/user name.
+  projectName: 'docus', // Usually your repo name.
 
   presets: [
     [
