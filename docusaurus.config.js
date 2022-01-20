16696a86ae4618d6366f3af60c4c466ea0ad2c80
Hooopo Wang <hoooopo@gmail.com>
2022-01-20 15:43:11 +0800

    fix broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,8 +91,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Explore',
+                to: '/docs/database/year-2021',
               },
             ],
           },
