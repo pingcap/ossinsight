e7d7b5adf52bb9868d78a1f60ec24bb7fe5c6259
Jagger <634750802@qq.com>
2022-02-16 23:48:01 +0800

    update schema

--- queries/events-history/template.sql
+++ queries/events-history/template.sql
@@ -6,6 +6,7 @@ FROM github_events_old AS github_events
          JOIN db_repos AS repo_subset
               ON repo_subset.id = github_events.repo_id
 WHERE github_events.type = 'WatchEvent'
+  AND github_events.event_year >= YEAR(DATE_SUB(NOW(), INTERVAL 7 YEAR))
 GROUP BY github_events.repo_id
 ORDER BY `count` DESC
 LIMIT 10;
