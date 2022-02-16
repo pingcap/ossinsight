2c73579c06d781ccd5762f8f37f0ec658df34ae1
Jagger <634750802@qq.com>
2022-02-17 00:14:36 +0800

    fix: template

--- queries/events-history/template.sql
+++ queries/events-history/template.sql
@@ -1,7 +1,7 @@
 SELECT
 /*+ read_from_storage(tiflash[github_events]) */
-    ANY_VALUE(repo_subset.name) AS events,
-    COUNT(*)                    AS `count`
+    ANY_VALUE(repo_subset.name) AS repo_name,
+    COUNT(*)                    AS events_count
 FROM github_events_old AS github_events
          JOIN db_repos AS repo_subset
               ON repo_subset.id = github_events.repo_id
