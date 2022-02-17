73356b73201419ffbf4e66f98a267a91c8394b3c
Jagger <634750802@qq.com>
2022-02-17 09:34:45 +0800

    new queries

---
+++ queries/events-per-year/template.sql
@@ -0,0 +1,12 @@
+SELECT
+/*+ read_from_storage(tiflash[github_events]) */
+    ANY_VALUE(repo_subset.name) AS repo_name,
+    COUNT(*)                    AS events_count
+FROM github_events_old AS github_events
+         JOIN db_repos AS repo_subset
+              ON repo_subset.id = github_events.repo_id
+WHERE github_events.type = 'WatchEvent'
+  AND github_events.event_year = 2021
+GROUP BY github_events.repo_id
+ORDER BY events_count DESC
+LIMIT 10;
