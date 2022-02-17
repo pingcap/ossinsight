cb4f7223668703278447bc4e26a8863aa95810a9
Jagger <634750802@qq.com>
2022-02-17 10:55:18 +0800

    update contributor quries

--- queries/contributors-per-year/template.sql
+++ queries/contributors-per-year/template.sql
@@ -1,14 +1,15 @@
 SELECT
 /*+ read_from_storage(tiflash[github_events]) */
     ANY_VALUE(repo_subset.name) AS repo_name,
-    COUNT(users.id)             AS events_count
+    COUNT(DISTINCT users.id) AS events_count
 FROM github_events_old AS github_events
          JOIN db_repos AS repo_subset
               ON repo_subset.id = github_events.repo_id
          JOIN users
               ON users.id = github_events.actor_id
 WHERE github_events.type = 'PullRequestEvent'
-  AND github_events.action = 'opened'
+  AND github_events.action = 'closed'
+  AND github_events.pr_merged IS TRUE
   AND github_events.event_year = 2021
 GROUP BY github_events.repo_id
 ORDER BY events_count DESC
