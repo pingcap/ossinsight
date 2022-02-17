90e2654a802a6e59223fb2d75bd1d201709752ad
Jagger <634750802@qq.com>
2022-02-17 11:08:44 +0800

    fix schema

--- queries/contributors-history/template.sql
+++ queries/contributors-history/template.sql
@@ -1,7 +1,7 @@
 SELECT
 /*+ read_from_storage(tiflash[github_events]) */
     ANY_VALUE(repo_subset.name) AS repo_name,
-    COUNT(DISTINCT github_events.actor_id) AS events_count
+    COUNT(DISTINCT github_events.actor_id) AS contributors_count
 FROM github_events_old AS github_events
          JOIN db_repos AS repo_subset
               ON repo_subset.id = github_events.repo_id
@@ -10,5 +10,5 @@ WHERE github_events.type = 'PullRequestEvent'
   AND github_events.pr_merged IS TRUE
   AND github_events.event_year >= YEAR(DATE_SUB(NOW(), INTERVAL 7 YEAR))
 GROUP BY github_events.repo_id
-ORDER BY events_count DESC
+ORDER BY contributors_count DESC
 LIMIT 10;
