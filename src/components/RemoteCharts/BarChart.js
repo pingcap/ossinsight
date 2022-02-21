d710964f588b00222165d472eee7cd0f29f9dd55
Jagger <634750802@qq.com>
2022-02-21 13:47:21 +0800

    improve chart theme

--- src/components/RemoteCharts/BarChart.js
+++ src/components/RemoteCharts/BarChart.js
@@ -4,6 +4,7 @@ import {BarChart,} from 'echarts/charts';
 import {GridComponent, TitleComponent, TooltipComponent,} from 'echarts/components';
 import {CanvasRenderer,} from 'echarts/renderers';
 import * as echarts from "echarts/core";
+import useThemeContext from '@theme/hooks/useThemeContext';
 
 // Register the required components
 echarts.use(
@@ -21,7 +22,7 @@ export default function ({data, loading, clear, size, n, deps, categoryIndex, va
       },
       grid: {
         containLabel: true,
-        left: 0,
+        left: clear ? 0 : 8,
         top: clear ? 0 : 16,
         bottom: clear ? 0 : 16
       },
@@ -46,17 +47,34 @@ export default function ({data, loading, clear, size, n, deps, categoryIndex, va
           data: data.map(d => d[valueIndex]),
           type: 'bar',
           barWidth: clear ? size / 2 : size,
-          itemStyle: {
-            color: '#ffe39f',
-            borderColor: '#786837',
-            opacity: 0.7
-          }
         }
       ]
     }
-  }, [data, ...deps])
+  }, [data, ...deps, categoryIndex, valueIndex, size, clear])
 
   const height = loading ? 400 : n * (size * (clear ? 1 : 1.5))
+
+  const opts = useMemo(() => {
+    return {
+      devicePixelRatio: window?.devicePixelRatio ?? 1,
+      renderer: 'canvas',
+      height: height,
+      width: 'auto',
+      locale: 'en'
+    }
+  }, [height])
+
+  const onEvents = useMemo(() => {
+    return {
+      'click': params => {
+        if ('name' in params) {
+          window.open(`https://github.com/${params.name}`).then()
+        }
+      }
+    }
+  }, [])
+
+  const {isDarkTheme} = useThemeContext();
 
   return (
     <ReactEChartsCore
@@ -65,26 +83,159 @@ export default function ({data, loading, clear, size, n, deps, categoryIndex, va
       option={options}
       notMerge={true}
       lazyUpdate={true}
-      theme={"theme_name"}
+      theme={isDarkTheme ? 'dark' : 'vintage'}
       style={{
-        height
+        height,
+        marginBottom: 16,
+        borderRadius: 'var(--ifm-global-radius)',
+        overflow: 'hidden'
       }}
-      opts={{
-        devicePixelRatio: window?.devicePixelRatio ?? 1,
-        renderer: 'canvas',
-        height: height,
-        width: 'auto',
-        locale: 'en'
-      }}
-      onEvents={{
-        'click': params => {
-          if ('name' in params) {
-            window.open(`https://github.com/${params.name}`).then()
-          }
-        }
-      }}
+      opts={opts}
+      onEvents={onEvents}
     />
   )
+}
 
+
+function registerThemeVintage () {
+  const colorPalette = ['#d87c7c', '#919e8b', '#d7ab82', '#6e7074', '#61a0a8', '#efa18d', '#787464', '#cc7e63', '#724e58', '#4b565b'];
+  echarts.registerTheme('vintage', {
+    color: colorPalette,
+    backgroundColor: '#fef8ef',
+    graph: {
+      color: colorPalette
+    }
+  });
 
 }
+
+function registerThemeDark () {
+  const contrastColor = '#eee';
+  const axisCommon = function () {
+    return {
+      axisLine: {
+        lineStyle: {
+          color: contrastColor
+        }
+      },
+      axisTick: {
+        lineStyle: {
+          color: contrastColor
+        }
+      },
+      axisLabel: {
+        textStyle: {
+          color: contrastColor
+        }
+      },
+      splitLine: {
+        lineStyle: {
+          type: 'dashed',
+          color: '#aaa'
+        }
+      },
+      splitArea: {
+        areaStyle: {
+          color: contrastColor
+        }
+      }
+    };
+  };
+
+  const colorPalette = ['#dd6b66','#759aa0','#e69d87','#8dc1a9','#ea7e53','#eedd78','#73a373','#73b9bc','#7289ab', '#91ca8c','#f49f42'];
+  const theme = {
+    color: colorPalette,
+    backgroundColor: '#333',
+    tooltip: {
+      axisPointer: {
+        lineStyle: {
+          color: contrastColor
+        },
+        crossStyle: {
+          color: contrastColor
+        }
+      }
+    },
+    legend: {
+      textStyle: {
+        color: contrastColor
+      }
+    },
+    textStyle: {
+      color: contrastColor
+    },
+    title: {
+      textStyle: {
+        color: contrastColor
+      }
+    },
+    toolbox: {
+      iconStyle: {
+        normal: {
+          borderColor: contrastColor
+        }
+      }
+    },
+    dataZoom: {
+      textStyle: {
+        color: contrastColor
+      }
+    },
+    timeline: {
+      lineStyle: {
+        color: contrastColor
+      },
+      itemStyle: {
+        normal: {
+          color: colorPalette[1]
+        }
+      },
+      label: {
+        normal: {
+          textStyle: {
+            color: contrastColor
+          }
+        }
+      },
+      controlStyle: {
+        normal: {
+          color: contrastColor,
+          borderColor: contrastColor
+        }
+      }
+    },
+    timeAxis: axisCommon(),
+    logAxis: axisCommon(),
+    valueAxis: axisCommon(),
+    categoryAxis: axisCommon(),
+
+    line: {
+      symbol: 'circle'
+    },
+    graph: {
+      color: colorPalette
+    },
+    gauge: {
+      title: {
+        textStyle: {
+          color: contrastColor
+        }
+      }
+    },
+    candlestick: {
+      itemStyle: {
+        normal: {
+          color: '#FD1050',
+          color0: '#0CF49B',
+          borderColor: '#FD1050',
+          borderColor0: '#0CF49B'
+        }
+      }
+    }
+  };
+  theme.categoryAxis.splitLine.show = false;
+  echarts.registerTheme('dark', theme);
+}
+
+registerThemeVintage()
+registerThemeDark()
