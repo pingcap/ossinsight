9837331b78ca4ef18fdf4dd1b4d9326fb3f4a53e
Jagger <634750802@qq.com>
2022-02-21 14:07:37 +0800

    fix sql

--- queries/recent-events-rank/template.sql
+++ queries/recent-events-rank/template.sql
@@ -1,8 +1,8 @@
 SELECT ANY_VALUE(repo_name)    AS repo_name,
        COUNT(github_events.id) AS events
 FROM github_events AS github_events
-WHERE created_at < DATE_FORMAT(UTC_TIMESTAMP - INTERVAL 1 - 10 HOUR, '%Y-%m-%d %H:00:00')
-  AND created_at >= DATE_FORMAT(UTC_TIMESTAMP - INTERVAL 2 - 10 HOUR, '%Y-%m-%d %H:00:00')
+WHERE created_at < DATE_FORMAT(UTC_TIMESTAMP - INTERVAL 1 + 10 HOUR, '%Y-%m-%d %H:00:00')
+  AND created_at >= DATE_FORMAT(UTC_TIMESTAMP - INTERVAL 2 + 10 HOUR, '%Y-%m-%d %H:00:00')
 GROUP BY github_events.repo_id
 ORDER BY events DESC
 LIMIT 500;
