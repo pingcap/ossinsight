8f27a8a10cc5ad526dd9e89a5e9f5a20351be8aa
Jagger <634750802@qq.com>
2022-02-28 13:12:31 +0800

    feat: add top queries

---
+++ queries/rt-top10-by-prs/query.sql
@@ -0,0 +1,18 @@
+WITH
+    # Fetch max updated event time and compute the start and end time.
+    datetime_range
+        AS (SELECT DATE_FORMAT(MAX(created_at) - INTERVAL 1 HOUR, '%Y-%m-%d %H:00:00') AS start
+                 , DATE_FORMAT(MAX(created_at), '%Y-%m-%d %H:00:00')                   AS end
+            FROM github_events_old)
+
+SELECT db_repos.name AS repo_name,
+       COUNT(*)      AS num
+FROM github_events_old github_events
+         JOIN db_repos ON db_repos.id = github_events.repo_id
+WHERE created_at >= (SELECT start FROM datetime_range)
+  AND created_at < (SELECT end FROM datetime_range)
+  AND type = 'PullRequestEvent'
+  AND action = 'opened'
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10
