bf323e499742ac88f556757944d3d1af827b4b60
Mini256 <minianter@foxmail.com>
2022-03-02 14:48:56 +0800

    fix: sort the contributor country/area by cnt (#3)

--- queries/stars-map/template.sql
+++ queries/stars-map/template.sql
@@ -4,4 +4,4 @@ use index(index_github_events_on_repo_name)
 left join users u ON github_events_old.actor_login = u.login
 where repo_name = 'pingcap/tidb' and github_events_old.type = 'WatchEvent' and u.country_code is not null
 group by 1
-order by 2;
+order by 2 desc;
