78ae9afe2414f329ffb625522383f00856525765
Mini256 <minianter@foxmail.com>
2022-03-09 16:26:18 +0800

    feat: add rt zscore sql (#16)

---
+++ queries/rt-zscore/template.sql
@@ -0,0 +1,78 @@
+WITH
+    stars AS (
+        SELECT
+            /*+ read_from_storage(tiflash[github_events]) */
+            db.name AS repo_name,
+            COUNT(*) AS count
+        FROM github_events
+             JOIN db_repos db ON db.id = github_events.repo_id
+        WHERE type = 'WatchEvent'
+        GROUP BY 1
+    ),
+
+     prs AS (
+         SELECT
+             /*+ read_from_storage(tiflash[github_events]) */
+             db.name AS repo_name,
+             COUNT(*) AS count
+         FROM github_events
+              JOIN db_repos db ON db.id = github_events.repo_id
+         WHERE type = 'PullRequestEvent' and action = 'opened'
+         GROUP BY 1
+     ),
+
+     contributors AS (
+         SELECT
+             /*+ read_from_storage(tiflash[github_events]) */
+             db.name AS repo_name,
+             count(distinct actor_id) AS count
+         FROM github_events
+              JOIN db_repos db ON db.id = github_events.repo_id
+         WHERE type in (
+                        'IssuesEvent',
+                        'PullRequestEvent',
+                        'IssueCommentEvent',
+                        'PullRequestReviewCommentEvent',
+                        'CommitCommentEvent',
+                        'PullRequestReviewEvent')
+         GROUP BY 1
+     ),
+
+     raw AS (
+         SELECT
+             name,
+             stars.count AS star_count,
+             prs.count AS pr_count,
+             contributors.count as user_count
+         FROM db_repos
+              LEFT JOIN stars ON stars.repo_name = name
+              LEFT JOIN prs ON prs.repo_name = name
+              LEFT JOIN contributors ON contributors.repo_name = name
+     ),
+
+     zz_pr AS (
+         SELECT AVG(pr_count) AS mean, STDDEV(pr_count) AS sd FROM raw
+     ),
+
+     zz_star AS (
+         SELECT AVG(star_count) AS mean, STDDEV(star_count) AS sd FROM raw
+     ),
+
+     zz_user AS (
+         SELECT AVG(user_count) AS mean, STDDEV(user_count) AS sd FROM raw
+     )
+
+SELECT
+    name,
+   ((star_count - zz_star.mean) / zz_star.sd) +
+   ((user_count - zz_user.mean) / zz_user.sd) +
+   ((pr_count - zz_pr.mean) / zz_pr.sd) AS z_score,
+   ((star_count - zz_star.mean) / zz_star.sd) AS z_score_star,
+   ((user_count - zz_user.mean) / zz_user.sd) AS z_score_user,
+   ((pr_count - zz_pr.mean) / zz_pr.sd) AS z_score_pr
+FROM
+    raw,
+    zz_star,
+    zz_user,
+    zz_pr
+ORDER BY 2 DESC
