9fd4c2344a868dd2a134a3704c4826214855bdcc
Mini256 <minianter@foxmail.com>
2022-03-10 21:04:00 +0800

    feat: add benchmark (#17)

--- app/server.ts
+++ app/server.ts
@@ -20,7 +20,7 @@ export default async function server(router: Router<DefaultState, ContextExtends
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    queueLimit: 10
+    queueLimit: parseInt(process.env.QUEUE_LIMIT || '20'),
   })
   const tokens = (process.env.GH_TOKENS || '').split(',').map(s => s.trim()).filter(Boolean);
   const ghExecutor = new GhExecutor(tokens, redisClient)
---
+++ benchmark/README.md
@@ -0,0 +1,5 @@
+# Benchmark
+
+```bash
+wrk -t12 -c100 --latency -d30s -s pull-request-creators-map https://community-preview-contributor.tidb.io
+```
