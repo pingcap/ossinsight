2365c92b1dc4933cc7e17f2c90a508d1920b3572
Jagger <634750802@qq.com>
2022-03-20 23:55:31 +0800

    use radar chart

--- src/pages/compare/_sections/common/SomeNumbers.tsx
+++ src/pages/compare/_sections/common/SomeNumbers.tsx
@@ -1,21 +1,123 @@
-import CompareNumbers, {CompareNumbersContainer} from "../../../../components/RemoteCards/CompareNumbers";
-import React from "react";
+import React, {useMemo} from "react";
 import {Repo} from "../../../../components/CompareHeader/RepoSelector";
+import useSWR, {SWRResponse} from "swr";
+import {createHttpClient} from "../../../../lib/request";
+import * as echarts from 'echarts/core';
+import {RadarChart} from 'echarts/charts';
+import {LegendComponent, TitleComponent} from 'echarts/components';
+import {CanvasRenderer} from 'echarts/renderers';
+import {EChartsOption} from "echarts";
+import ReactECharts from "echarts-for-react";
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import useThemeContext from "@theme/hooks/useThemeContext";
+import {useTheme} from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import {formatNumber} from "../../../../lib/text";
+
+echarts.use([TitleComponent, LegendComponent, RadarChart, CanvasRenderer]);
 
 export interface SomeNumbersProps {
   repos: [Repo | null, Repo | null]
   queries: { title: string, query: string }[]
 }
 
+const httpClient = createHttpClient();
 
-const SomeNumbers = ({ repos, queries}: SomeNumbersProps) => {
+const SomeNumbers = ({repos, queries}: SomeNumbersProps) => {
+  const {isDarkTheme} = useThemeContext();
+  const theme = useTheme();
+  const isSmall = useMediaQuery(theme.breakpoints.down('sm'))
+
+  const r1 = useNumberQueries(queries.map(query => query.query), repos[0])
+  const r2 = useNumberQueries(queries.map(query => query.query), repos[1])
+
+  const option: EChartsOption = useMemo(() => {
+    return {
+      legend: {
+        left: '25%',
+        orient: 'vertical',
+        data: [repos[0]?.name ?? 'repo 1', repos[1]?.name ?? 'repo 2']
+      },
+      radar: {
+        // shape: 'circle',
+        indicator: queries.map(({title}, i) => ({
+          name: title,
+          max: Math.max(r1.data?.[i] ?? 0, r2.data?.[i] ?? 0, 1)
+        })),
+        axisName: {
+          padding: [8, 8]
+        },
+        center: ['50%', '50%'],
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      series: [
+        {
+          type: 'radar',
+          tooltip: {
+            trigger: 'item'
+          },
+          areaStyle: {},
+          data: repos.map((repo, i) => ({
+            value: [r1, r2][i].data,
+            name: repo?.name ?? `repo ${i + 1}`,
+            label: {
+              show: true,
+              formatter: function (params) {
+                return formatNumber(params.value, 1);
+              },
+            }
+          }))
+        }
+      ]
+    };
+  }, [r1.data, r2.data])
+
   return (
-    <CompareNumbersContainer>
-      {queries.map(({title, query}) => (
-        <CompareNumbers key={query} title={title} query={query} repos={repos} />
-      ))}
-    </CompareNumbersContainer>
+    <BrowserOnly>
+      {() => <ReactECharts
+        option={option}
+        notMerge={false}
+        lazyUpdate={true}
+        style={{
+          height: 'auto',
+          aspectRatio: isSmall ? '2 / 1' : '4 / 1',
+          overflow: 'hidden'
+        }}
+        theme={isDarkTheme ? 'dark' : 'vintage'}
+        opts={{
+          devicePixelRatio: window?.devicePixelRatio ?? 1,
+          renderer: 'canvas',
+          width: 'auto',
+          locale: 'en'
+        }}
+      />}
+    </BrowserOnly>
   )
+}
+
+function useNumberQueries(queries: string[], repo: Repo | null): SWRResponse<number[]> {
+  return useSWR([queries, repo], {
+    fetcher: (queries, repo) => {
+      if (!repo) {
+        return Promise.resolve(queries.map(() => 0))
+      } else {
+        return Promise.all(queries.map(async query => {
+          try {
+            const {data: {data}} = await httpClient.get(`/q/${query}`, {params: {repoId: repo.id}})
+            const origin = Object.values(data[0])[0] as string
+            return parseInt(origin)
+          } catch (e) {
+            return 0
+          }
+        }))
+      }
+    },
+    fallbackData: queries.map(() => 0) as number[],
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false
+  })
 }
 
 export default SomeNumbers
