02f4ff9b07bde4e584ab16544cabc74cad2ac638
Mini256 <minianter@foxmail.com>
2022-03-22 17:57:23 +0800

    feat: add rate limit

--- index.ts
+++ index.ts
@@ -5,11 +5,11 @@ import dotenv from 'dotenv';
 import consola, {Consola} from 'consola';
 import cors from '@koa/cors';
 
+const RateLimit = require('koa2-ratelimit').RateLimit;
+const Stores = require('koa2-ratelimit').Stores;
+
 const logger = consola.withTag('app')
-
-
 const {parsed} = dotenv.config()
-
 if (parsed) {
   logger.info('loaded env:', Object.keys(parsed).join(', '))
 }
@@ -22,12 +22,22 @@ export interface ContextExtends extends App.DefaultContext {
 
 const app = new App<App.DefaultState, ContextExtends>()
 const router = new Router<App.DefaultState, ContextExtends>()
+const rateLimitInterval = parseInt(process.env.RATE_LIMIT_INTERVAL || '1');
+const rateLimitMaxRequests = parseInt(process.env.RATE_LIMIT_MAX_REQUEST || '60');
+const limiter = RateLimit.middleware({
+  interval: { min: rateLimitInterval },    // 1 minutes = 1 * 60 * 1000
+  max: rateLimitMaxRequests,               // limit max requests per interval for each IP.
+  store: new Stores.Redis({
+    url: process.env.REDIS_URL
+  })
+});
 
 app.use(async (ctx, next) => {
   ctx.logger = logger
   await next()
 })
 app.use(cors({origin: '*'}))
+app.use(limiter)
 
 server(router)
 
