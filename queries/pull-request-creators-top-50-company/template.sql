e3e5691db798db38cf2b361d419d13aaaa39fcf3
Mini256 <minianter@foxmail.com>
2022-03-24 21:40:59 +0800

    fix: fix company name (#32)
    * fix: fix company name
    
    * fix
    
    * trigger

--- queries/pull-request-creators-top-50-company/template.sql
+++ queries/pull-request-creators-top-50-company/template.sql
@@ -1,14 +1,14 @@
 select *
 from (
-    select
-        trim(replace(replace(replace(replace(replace(replace(replace(replace(u.company, ',', ''), '-', ''), '@', ''), '.', ''), 'none', ''), 'inc', ''), 'com', ''), 'www', '')) as company_name,
-        count(distinct github_events.actor_id) as code_contributors
-    from github_events
-    use index(index_github_events_on_repo_id)
-    left join users u ON github_events.actor_login = u.login
-    where repo_id = 41986369 and github_events.type = 'PullRequestEvent' and action = 'opened'
-    group by 1
+     select
+         trim(replace(replace(replace(replace(replace(replace(replace(replace(lower(u.company), ',', ''), '-', ''), '@', ''), 'www.', ''), 'inc', ''), '.com', ''), '.cn', ''), '.', '')) as company_name,
+         count(distinct github_events.actor_id) as code_contributors
+     from github_events
+              use index(index_github_events_on_repo_id)
+          left join users u ON github_events.actor_login = u.login
+     where repo_id = 41986369 and github_events.type = 'PullRequestEvent' and action = 'opened'
+     group by 1
 ) sub
-where length(company_name) != 0 and company_name not in ('-', '--- click here ---', 'none', 'no', 'home', 'n/a')
+where length(company_name) != 0 and company_name not in ('-', 'none', 'no', 'home', 'n/a', 'null')
 order by code_contributors desc
 limit 50;
--- queries/stars-common-company/template.sql
+++ queries/stars-common-company/template.sql
@@ -2,7 +2,7 @@ select
     company_name
 from (
      select
-         trim(replace(replace(replace(replace(replace(replace(replace(replace(u.company, ',', ''), '-', ''), '@', ''), '.', ''), 'ltd', ''), 'inc', ''), 'com', ''), 'www', '')) as company_name,
+         trim(replace(replace(replace(replace(replace(replace(replace(replace(lower(u.company), ',', ''), '-', ''), '@', ''), 'www.', ''), 'inc', ''), '.com', ''), '.cn', ''), '.', '')) as company_name,
          repo_id,
          count(distinct actor_id) as stargazers
      from github_events
@@ -10,8 +10,8 @@ from (
      left join users u ON github_events.actor_login = u.login
      where repo_id IN (48833910, 156018) and github_events.type = 'WatchEvent'
      group by 1, 2
- ) sub
-where length(company_name) != 0 and company_name not in ('-', '--- click here ---', 'none', 'no', 'home', 'n/a')
+) sub
+where length(company_name) != 0 and company_name not in ('-', 'none', 'no', 'home', 'n/a', 'null')
 group by 1
 having COUNT(distinct repo_id) > 1
 order by SUM(stargazers) desc
