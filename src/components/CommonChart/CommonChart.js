ff810c397ed63ced74347edfa52c8f068a5a2f15
Jagger <634750802@qq.com>
2022-03-25 13:58:55 +0800

    insight page support dynamic load

--- src/components/CommonChart/CommonChart.js
+++ src/components/CommonChart/CommonChart.js
@@ -5,9 +5,12 @@ import DateAdapter from '@mui/lab/AdapterLuxon';
 import Head from '@docusaurus/Head';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import ThemeAdaptor from "../ThemeAdaptor";
+import {useInView} from "react-intersection-observer";
+import InViewContext from '../InViewContext'
 
 
 function CommonChart({chart: rawChart, noSearch, ...rest}) {
+  const { inView, ref } = useInView({ fallbackInView: true })
   const chart = useMemo(() => {
     if (typeof rawChart === 'string') {
       return require('../RemoteCharts/' + rawChart + '/index.js').default
@@ -32,9 +35,13 @@ function CommonChart({chart: rawChart, noSearch, ...rest}) {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
         </Head>
-        {form}
-        {form && <Divider sx={{my: 2}} />}
-        {child}
+        <div ref={ref}>
+          <InViewContext.Provider value={{ inView }}>
+            {form}
+            {form && <Divider sx={{my: 2}} />}
+            {child}
+          </InViewContext.Provider>
+        </div>
       </ThemeAdaptor>
     </LocalizationProvider>
   )
