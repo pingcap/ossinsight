57ff0ff89d6269e88a8381f097ad1681ca50822f
sykp241095 <sykp241095@gmail.com>
2022-04-15 13:44:19 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
 //       announcementBar: {
 //         id: 'announcement-20220414',
 //         content:
-//           'TiDB 6.0 is released, go to read the <a target="_blank" rel="noopener noreferrer" href="https://docs.pingcap.com/tidb/v6.0/release-6.0.0-dmr">release notes</a> 🎉🎉🎉',
+//           'TiDB 6.0 is released, go to read the <a target="_blank" rel="noopener noreferrer" href="https://docs.pingcap.com/tidb/v6.0/release-6.0.0-dmr">release notes</a> 🎉',
 //         backgroundColor: '#fafbfc',
 //         textColor: '#091E42',
 //         isCloseable: false,
