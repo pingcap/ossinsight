8f27223587cd33704e3dff9e9b4b843622b25441
sykp241095 <sykp241095@gmail.com>
2022-04-15 11:20:05 +0800

    Update docusaurus.config.js (#118)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,14 +65,14 @@ const config = {
         disableSwitch: false,
         respectPrefersColorScheme: false,
       },
-      announcementBar: {
-        id: 'announcement-20220414',
-        content:
-          'TiDB 6.0 is released, go to read the <a target="_blank" rel="noopener noreferrer" href="https://docs.pingcap.com/tidb/v6.0/release-6.0.0-dmr">release notes</a> 🎉🎉🎉',
-        backgroundColor: '#fafbfc',
-        textColor: '#091E42',
-        isCloseable: false,
-      },
+//       announcementBar: {
+//         id: 'announcement-20220414',
+//         content:
+//           'TiDB 6.0 is released, go to read the <a target="_blank" rel="noopener noreferrer" href="https://docs.pingcap.com/tidb/v6.0/release-6.0.0-dmr">release notes</a> 🎉🎉🎉',
+//         backgroundColor: '#fafbfc',
+//         textColor: '#091E42',
+//         isCloseable: false,
+//       },
       navbar: {
         title: 'OSS Insight',
         logo: {
