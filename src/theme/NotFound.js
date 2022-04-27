9b120b2ed85028cef87064d2b885e4546202cd77
Jagger <634750802@qq.com>
2022-04-27 10:54:10 +0800

    try redirect

---
+++ src/theme/NotFound.js
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React, {useEffect} from 'react';
+import Layout from '@theme/Layout';
+import Translate, {translate} from '@docusaurus/Translate';
+
+function NotFound() {
+
+  useEffect(() => {
+
+    // Incase browser doesn't support window.location.origin
+    function getOrigin() {
+      if (window.location.origin) return window.location.origin;
+      return window.location.protocol + "//" + window.location.hostname +
+        (window.location.port ? ':' + window.location.port : '');
+    }
+
+    const pathname = encodeURIComponent(window.location.pathname.split('/').join('%2F'));
+    // Only support redirect analyze/repo/name for now
+    if (!/^\/analyze\/[^\/?#]+\/[^\/?#]+\/?[?#]/.test(pathname)) {
+      return
+    }
+    const query = encodeURIComponent(window.location.search.slice(1));
+    const hash = encodeURIComponent(window.location.hash.slice(1));
+    const origin = getOrigin();
+
+    const redirectTo = origin + '/redirect?' +
+      'redirect=true' +
+      '&pathname=' + pathname +
+      '&query=' + query +
+      '&hash=' + hash;
+
+    window.location.replace(redirectTo)
+  }, [])
+
+  return (
+    <Layout
+      title={translate({
+        id: 'theme.NotFound.title',
+        message: 'Page Not Found',
+      })}>
+      <main className="container margin-vert--xl">
+        <div className="row">
+          <div className="col col--6 col--offset-3">
+            <h1 className="hero__title">
+              <Translate
+                id="theme.NotFound.title"
+                description="The title of the 404 page">
+                Page Not Found
+              </Translate>
+            </h1>
+            <p>
+              <Translate
+                id="theme.NotFound.p1"
+                description="The first paragraph of the 404 page">
+                We could not find what you were looking for.
+              </Translate>
+            </p>
+            <p>
+              <Translate
+                id="theme.NotFound.p2"
+                description="The 2nd paragraph of the 404 page">
+                Please contact the owner of the site that linked you to the
+                original URL and let them know their link is broken.
+              </Translate>
+            </p>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
+
+export default NotFound;
