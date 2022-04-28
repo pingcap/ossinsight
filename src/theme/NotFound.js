00bac6b628a1eff06cff0011a2166ac8f4518f15
Jagger <634750802@qq.com>
2022-04-28 23:00:42 +0800

    optimize 404

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -31,36 +31,54 @@ function NotFound() {
         id: 'theme.NotFound.title',
         message: 'Page Not Found',
       }) : 'Redirecting...'}>
-      <main className="container margin-vert--xl" style={{display: display ? 'block' : 'hidden'}}>
-        <div className="row">
-          <div className="col col--6 col--offset-3">
-            <h1 className="hero__title">
-              <Translate
-                id="theme.NotFound.title"
-                description="The title of the 404 page">
-                Page Not Found
-              </Translate>
-            </h1>
-            <p>
-              <Translate
-                id="theme.NotFound.p1"
-                description="The first paragraph of the 404 page">
-                We could not find what you were looking for.
-              </Translate>
-            </p>
-            <p>
-              <Translate
-                id="theme.NotFound.p2"
-                description="The 2nd paragraph of the 404 page">
-                Please contact the owner of the site that linked you to the
-                original URL and let them know their link is broken.
-              </Translate>
-            </p>
-          </div>
-        </div>
-      </main>
+      {display ? renderNotFound() : renderRedirect()}
     </Layout>
   );
+}
+
+function renderRedirect () {
+  return (
+    <main className="container margin-vert--xl">
+      <div className="row">
+        <div className="col col--6 col--offset-3">
+          <h1>Redirecting...</h1>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+function renderNotFound () {
+  return (
+    <main className="container margin-vert--xl">
+      <div className="row">
+        <div className="col col--6 col--offset-3">
+          <h1 className="hero__title">
+            <Translate
+              id="theme.NotFound.title"
+              description="The title of the 404 page">
+              Page Not Found
+            </Translate>
+          </h1>
+          <p>
+            <Translate
+              id="theme.NotFound.p1"
+              description="The first paragraph of the 404 page">
+              We could not find what you were looking for.
+            </Translate>
+          </p>
+          <p>
+            <Translate
+              id="theme.NotFound.p2"
+              description="The 2nd paragraph of the 404 page">
+              Please contact the owner of the site that linked you to the
+              original URL and let them know their link is broken.
+            </Translate>
+          </p>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default NotFound;
