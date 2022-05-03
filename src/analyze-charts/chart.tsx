97dd89c712bc4026206dc3ac495c42ae459b3bb6
Jagger <634750802@qq.com>
2022-05-04 03:50:44 +0800

    share charts

--- src/analyze-charts/chart.tsx
+++ src/analyze-charts/chart.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import ShareIcon from '@mui/icons-material/Share';
+import Fab from '@mui/material/Fab';
+import EChartsReact from 'echarts-for-react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+import { CommonChartContext } from '../components/CommonChart';
+import { CommonChartShareInfo } from '../components/CommonChart/context';
+import ShareDialog from '../components/ShareDialog';
 import {AnalyzeChartContextProps, AnalyzeContextProps, useAnalyzeChartContext, useAnalyzeContext} from './context';
 import {EChartsOption} from 'echarts';
 import ECharts, {EChartsProps} from '../components/ECharts';
@@ -8,6 +14,31 @@ export function withChart<T = unknown, P = {}>(useOption: (props: AnalyzeContext
   return (props: Omit<EChartsProps, 'option'> & { spec?: P }) => {
     const context = useAnalyzeContext();
     const chartContext = useAnalyzeChartContext<T>();
+
+    const [showDebugModel, setShowDebugModel] = useState(false);
+    const echartsRef = useRef<EChartsReact>()
+
+    const handleShowDebugModel = useCallback(() => {
+      setShowDebugModel(true);
+    }, [])
+
+    const handleCloseDebugModel = useCallback(() => {
+      setShowDebugModel(false);
+    }, [])
+
+    const shareBtnDisabled = useMemo(() => {
+      return !!(chartContext.data.loading || chartContext.data.error || chartContext.compareData.loading || chartContext.compareData.error)
+    }, [chartContext.data.loading, chartContext.compareData.loading, chartContext.data.error, chartContext.compareData.error])
+
+    const shareInfo: CommonChartShareInfo = {
+      title: chartContext.title,
+      description: chartContext.description,
+      keywords: [],
+      hash: chartContext.hash,
+      message: context.comparingRepoName
+        ? `I'm comparing ${context.repoName} vs ${context.comparingRepoName} at OSSInsight.io`
+        : `I'm analyzing ${context.repoName} at OSSInsight.io`,
+    }
 
     const ctx = {...context, ...chartContext, context: {} as Record<string, any>};
 
@@ -19,13 +50,35 @@ export function withChart<T = unknown, P = {}>(useOption: (props: AnalyzeContext
     dangerousSetCtx(undefined);
 
     return (
-      <ECharts
-        option={option}
-        {...defaultProps}
-        {...props}
-        notMerge
-        lazyUpdate
-      />
+      <div style={{ position: 'relative' }}>
+        <CommonChartContext.Provider value={{shareInfo}}>
+          <ECharts
+            option={option}
+            {...defaultProps}
+            {...props}
+            notMerge
+            lazyUpdate
+            ref={echartsRef}
+          />
+          <Fab
+            size='small'
+            sx={{
+              position: 'absolute',
+              right: 24,
+              top: 24,
+              zIndex: 'var(--ifm-z-index-fixed-mui)',
+              opacity: 0.2,
+              ':hover': {
+                opacity: 1
+              }
+          }}
+            onClick={handleShowDebugModel} disabled={shareBtnDisabled}
+          >
+            <ShareIcon />
+          </Fab>
+          <ShareDialog open={showDebugModel} onClose={handleCloseDebugModel} />
+        </CommonChartContext.Provider>
+      </div>
     );
   };
 }
