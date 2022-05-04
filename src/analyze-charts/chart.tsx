80332844cac9ed87c22b9b6da3aa74590a180628
Jagger <634750802@qq.com>
2022-05-04 09:30:45 +0800

    fix share

--- src/analyze-charts/chart.tsx
+++ src/analyze-charts/chart.tsx
@@ -2,12 +2,11 @@ import ShareIcon from '@mui/icons-material/Share';
 import Fab from '@mui/material/Fab';
 import EChartsReact from 'echarts-for-react';
 import React, { useCallback, useMemo, useRef, useState } from 'react';
-import { CommonChartContext } from '../components/CommonChart';
-import { CommonChartShareInfo } from '../components/CommonChart/context';
+import CommonChartContext, { CommonChartShareInfo } from '../components/CommonChart/context';
 import ShareDialog from '../components/ShareDialog';
 import {AnalyzeChartContextProps, AnalyzeContextProps, useAnalyzeChartContext, useAnalyzeContext} from './context';
 import {EChartsOption} from 'echarts';
-import ECharts, {EChartsProps} from '../components/ECharts';
+import ECharts, { EChartsContext, EChartsProps } from '../components/ECharts';
 import {dangerousSetCtx} from './options/_danger';
 
 export function withChart<T = unknown, P = {}>(useOption: (props: AnalyzeContextProps & AnalyzeChartContextProps<T>, chartProps?: P) => EChartsOption, defaultProps: Partial<Omit<EChartsProps, 'option'>> = {}) {
@@ -52,31 +51,33 @@ export function withChart<T = unknown, P = {}>(useOption: (props: AnalyzeContext
     return (
       <div style={{ position: 'relative' }}>
         <CommonChartContext.Provider value={{shareInfo}}>
-          <ECharts
-            option={option}
-            {...defaultProps}
-            {...props}
-            notMerge
-            lazyUpdate
-            ref={echartsRef}
-          />
-          <Fab
-            size='small'
-            sx={{
-              position: 'absolute',
-              right: 24,
-              top: 24,
-              zIndex: 'var(--ifm-z-index-fixed-mui)',
-              opacity: 0.2,
-              ':hover': {
-                opacity: 1
-              }
-          }}
-            onClick={handleShowDebugModel} disabled={shareBtnDisabled}
-          >
-            <ShareIcon />
-          </Fab>
-          <ShareDialog open={showDebugModel} onClose={handleCloseDebugModel} />
+          <EChartsContext.Provider value={{echartsRef}}>
+            <ECharts
+              option={option}
+              {...defaultProps}
+              {...props}
+              notMerge
+              lazyUpdate
+              ref={echartsRef}
+            />
+            <Fab
+              size='small'
+              sx={{
+                position: 'absolute',
+                right: 24,
+                top: 24,
+                zIndex: 'var(--ifm-z-index-fixed-mui)',
+                opacity: 0.2,
+                ':hover': {
+                  opacity: 1
+                }
+              }}
+              onClick={handleShowDebugModel} disabled={shareBtnDisabled}
+            >
+              <ShareIcon />
+            </Fab>
+            <ShareDialog open={showDebugModel} onClose={handleCloseDebugModel} />
+          </EChartsContext.Provider>
         </CommonChartContext.Provider>
       </div>
     );
