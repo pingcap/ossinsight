0d19f007dfea1ff63f498ef8cbf3adb073cbadbe
Mini256 <minianter@foxmail.com>
2022-05-07 22:04:43 +0800

    fix: distinct star by actor_id

--- queries/stars-history/template.sql
+++ queries/stars-history/template.sql
@@ -1,9 +1,9 @@
 WITH prs_with_latest_repo_name AS (
     SELECT
         event_month,
-        actor_login,
+        actor_id,
         FIRST_VALUE(repo_name) OVER (PARTITION BY repo_id ORDER BY created_at DESC) AS repo_name,
-        ROW_NUMBER() OVER(PARTITION BY actor_login) AS row_num
+        ROW_NUMBER() OVER(PARTITION BY actor_id) AS row_num
     FROM github_events
     USE INDEX(index_github_events_on_repo_id)
     WHERE
@@ -12,7 +12,7 @@ WITH prs_with_latest_repo_name AS (
     SELECT
         event_month,
         repo_name,
-        COUNT(actor_login) OVER(PARTITION BY repo_name ORDER BY event_month ASC) AS total
+        COUNT(actor_id) OVER(PARTITION BY repo_name ORDER BY event_month ASC) AS total
     FROM prs_with_latest_repo_name
     WHERE row_num = 1
     ORDER BY 1
