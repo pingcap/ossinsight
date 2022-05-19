ee9a7f013fbc49432bbf72f2e1bf0720bc79514d
sykp241095 <sykp241095@gmail.com>
2022-05-19 16:55:25 +0800

    Update dimensions.tsx

--- src/dynamic-pages/collections/dimensions.tsx
+++ src/dynamic-pages/collections/dimensions.tsx
@@ -19,8 +19,8 @@ const PrCreatorIcon = ({ size }: { size: number }) => (
 
 const dimensions: Dimension[] = [
   { title: 'Stars', key: 'stars', prefix: 'stars', icon: <StarIcon size={18} /> },
-  { title: 'PRs', key: 'pull-requests', prefix: 'prs', icon: <GitMergeIcon size={18}/>},
-  { title: 'PR Creators', key: 'pull-request-creators', icon: <PrCreatorIcon size={18}/> },
+  { title: 'Pull Requests', key: 'pull-requests', prefix: 'prs', icon: <GitMergeIcon size={18}/>},
+  { title: 'Pull Request Creators', key: 'pull-request-creators', icon: <PrCreatorIcon size={18}/> },
   { title: 'Issues', key: 'issues', prefix: 'issues', icon: <IssueOpenedIcon size={18}/>},
 ]
 
