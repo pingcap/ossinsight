d0a74f3b8be64bd45eaa824068b722ec58146aa1
sykp241095 <sykp241095@gmail.com>
2022-05-20 22:18:21 +0800

    Fix url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,19 +75,19 @@ const config = {
             to: '/blog/deep-insight-into-open-source-databases',
           },
           {
-            from: '/js-framework/deep-insight-into-js-framework',
+            from: '/js-framework/deep-insight-into-js-framework-2021',
             to: '/blog/deep-insight-into-js-framework-2021',
           },
           {
-            from: '/language/deep-insight-into-programming-languages',
+            from: '/language/deep-insight-into-programming-languages-2021',
             to: '/blog/deep-insight-into-programming-languages-2021',
           },
           {
-            from: '/low-code/deep-insight-into-lowcode-development-tools',
+            from: '/low-code/deep-insight-into-lowcode-development-tools-2021',
             to: '/blog/deep-insight-into-lowcode-development-tools-2021',
           },
           {
-            from: '/web-framework/deep-insight-about-web-framework',
+            from: '/web-framework/deep-insight-about-web-framework-2021',
             to: '/blog/deep-insight-into-web-framework-2021',
           },
         ]
