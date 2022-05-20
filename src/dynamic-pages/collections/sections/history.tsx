838122cc054338feb0e58b21e5b14b6dc2f4d0e6
Jagger <634750802@qq.com>
2022-05-20 14:08:43 +0800

    use top 10 data

--- src/dynamic-pages/collections/sections/history.tsx
+++ src/dynamic-pages/collections/sections/history.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
 import { use } from 'echarts/core';
 import { CanvasRenderer } from 'echarts/renderers';
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
 import { withInViewContainer } from '../../../components/InViewContainer';
 import Watermark from '../components/Watermark';
 import CollectionsContext from '../context';
@@ -26,6 +26,29 @@ export default withInViewContainer(function HistorySection() {
   const { dimension, tabs } = useDimensionTabs();
   const asyncData = useCollectionHistory(collection?.id, dimension.key);
 
+  const top10Data: typeof asyncData = useMemo(() => {
+    if (!asyncData.data) {
+      return asyncData
+    }
+    const maxTime = asyncData.data.data.reduce((set, item) => {
+      return set < item.event_month ? item.event_month : set
+    }, '')
+    const top10Names = asyncData.data.data
+      .filter(item => item.event_month === maxTime)
+      .sort((a, b) =>b.total - a.total)
+      .slice(0, 10)
+      .map(item => item.repo_name)
+      .reduce((set, item) => set.add(item), new Set<string>())
+    return {
+      data: {
+        ...asyncData.data,
+        data: asyncData.data.data.filter(item => top10Names.has(item.repo_name)),
+      },
+      loading: asyncData.loading,
+      error: asyncData.error,
+    }
+  }, [asyncData.data, asyncData.error, asyncData.loading])
+
   return (
     <section>
       <H2>Historical Trending</H2>
@@ -33,7 +56,7 @@ export default withInViewContainer(function HistorySection() {
       {tabs}
       <br />
       {withRemote(
-        asyncData,
+        top10Data,
         data => (
           <LineChart
             theme="dark"
@@ -43,7 +66,7 @@ export default withInViewContainer(function HistorySection() {
             fields={{ name: 'repo_name', time: 'event_month', value: 'total' }}
             formatTime={formatTime}
           >
-            <Title id='title' text={`${collection.name} ${dimension.title} historical trending`}/>
+            <Title id='title' text={`Top 10 ${collection.name} ${dimension.title} historical trending`}/>
             <Watermark left='10%' top='10%' />
           </LineChart>
         ),
