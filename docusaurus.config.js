d275cc5e51bdb7f7d8a74a24e66773a558326729
sykp241095 <sykp241095@gmail.com>
2022-06-15 21:41:28 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -241,9 +241,6 @@ const config = {
                 label: 'TiDB Cloud',
                 href: 'https://en.pingcap.com/tidb-cloud?utm_source=ossinsight&utm_medium=referral',
               },
-              {
-                html: '<br /><br /><b>Location</b><p style="color:grey">California, USA<br />1250 Borregas Ave, Office 131<br />Sunnyvale, CA 94089<br />+1 650 382 9973</p>',
-              },
             ],
           },
           {
@@ -289,6 +286,9 @@ const config = {
               {
                 label: 'GitHub',
                 href: 'https://github.com/pingcap/ossinsight',
+              },
+              {
+                html: '<br /><br /><b>Location</b><p style="color:grey">California, USA<br />1250 Borregas Ave, Office 131<br />Sunnyvale, CA 94089<br />+1 650 382 9973</p>',
               },
             ],
           },
