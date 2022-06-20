8e25337443c9915a0ea97cd3bd9163d4f28fc3cd
Mini256 <minianter@foxmail.com>
2022-06-20 10:55:02 +0800

    fix: add bot queries (#55)
    * fix: add onlyFromCache flag
    
    * fix: change event_month to event_year
    
    * query: add dependabot commit time distribution query
    
    * query: add bot contribution ranking query
    
    * fix: only calc bot contribution
    
    * fix
    
    * feat: add weird bots query
    
    * fix

--- queries/bot-total-history/template.sql
+++ queries/bot-total-history/template.sql
@@ -1,6 +1,6 @@
 WITH bots_with_first_seen AS (
     SELECT
-        actor_login, MIN(event_month) AS first_seen_at
+        actor_login, MIN(event_year) AS first_seen_at
     FROM github_events ge
     WHERE
         actor_login REGEXP '^(bot-.+|.+bot|.+\\[bot\\]|.+-bot-.+|robot-.+|.+-ci-.+|.+-ci|.+-testing|.+clabot.+|.+-gerrit|k8s-.+|.+-machine|.+-automation|github-.+|.+-github|.+-service|.+-builds|codecov-.+|.+teamcity.+|jenkins-.+|.+-jira-.+)$'
@@ -9,12 +9,12 @@ WITH bots_with_first_seen AS (
 ),  acc AS (
     SELECT
         COUNT(actor_login) OVER (ORDER BY first_seen_at) AS cnt,
-        first_seen_at AS event_month
+        first_seen_at AS event_year
     FROM
         bots_with_first_seen AS bwfs
-    ORDER BY event_month
+    ORDER BY event_year
 )
-SELECT ANY_VALUE(cnt) AS bots_total, event_month
+SELECT ANY_VALUE(cnt) AS bots_total, event_year
 FROM acc
-GROUP BY event_month
-ORDER BY event_month
+GROUP BY event_year
+ORDER BY event_year
