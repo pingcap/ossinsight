faf5b22ef21388e0d5ba80f8c227bd480a9c965a
sykp241095 <sykp241095@gmail.com>
2022-06-21 08:12:07 +0800

    Use short url

---
+++ src/pages/s.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Redirect} from '@docusaurus/router';
+
+export default function Compare() {
+  return <Redirect to="/?utm_source=LinkedIn&utm_medium=promotion&utm_campaign=open_source" />;
+};
