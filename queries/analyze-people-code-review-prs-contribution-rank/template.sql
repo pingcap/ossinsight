0ee133602d7341385830a3be2c4202006d80bd79
Mini256 <minianter@foxmail.com>
2022-06-22 17:46:55 +0800

    fix: fix review prs contribution rank alias name

--- queries/analyze-people-code-review-prs-contribution-rank/template.sql
+++ queries/analyze-people-code-review-prs-contribution-rank/template.sql
@@ -31,7 +31,7 @@ SELECT
     lm.prs AS last_month_events,
     COALESCE(l2m.prs, 0) AS last_2nd_month_events,
     COALESCE(lm.prs - l2m.prs, 0) AS changes, 
-    lm.prs / (SELECT COALESCE(SUM(prs), 0) FROM review_contribution_last_month) AS review_proportion
+    lm.prs / (SELECT COALESCE(SUM(prs), 0) FROM review_contribution_last_month) AS proportion
 FROM review_contribution_last_month lm
 LEFT JOIN review_contribution_last_2nd_month l2m ON lm.actor_id = l2m.actor_id
 ORDER BY lm.prs DESC
