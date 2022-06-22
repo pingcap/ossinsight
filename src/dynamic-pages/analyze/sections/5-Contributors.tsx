1cbc527bcc64535dba9c86232c347dbf7cec27a0
Jagger <634750802@qq.com>
2022-06-22 17:46:54 +0800

    fix

--- src/dynamic-pages/analyze/sections/5-Contributors.tsx
+++ src/dynamic-pages/analyze/sections/5-Contributors.tsx
@@ -212,7 +212,7 @@ export const Contributors = forwardRef(function ({}, ref: ForwardedRef<HTMLEleme
 
   const selectDescriptor = (
     <FormControl variant="standard">
-      <InputLabel id='select-descriptor-label'>Dimension</InputLabel>
+      <InputLabel id='select-descriptor-label'>Metrics</InputLabel>
       <Select<Descriptor<any>>
         labelId='select-descriptor-label'
         id='select-descriptor'
