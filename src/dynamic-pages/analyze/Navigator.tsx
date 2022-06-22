9487660917e7a20902805c54a621bfeb923551b7
Jagger <634750802@qq.com>
2022-06-22 22:17:25 +0800

    feat: analyze contributors monthly (#435)
    Add simple monthly report for single repo

--- src/dynamic-pages/analyze/Navigator.tsx
+++ src/dynamic-pages/analyze/Navigator.tsx
@@ -1,16 +1,17 @@
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import AutoGraphIcon from '@mui/icons-material/AutoGraph';
 import { BottomNavigation, BottomNavigationAction, useEventCallback } from '@mui/material';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
-import { GitCommitIcon, GitPullRequestIcon, IssueClosedIcon, PersonIcon } from '@primer/octicons-react';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 const SideContainer = styled('div')({
   width: '100%',
-  height: 'calc(100vh - var(--ifm-navbar-height) - 22px)',
+  height: 'calc(100vh - var(--ifm-navbar-height))',
   backgroundColor: '#242526',
 });
 
@@ -20,80 +21,114 @@ const ColorBox = styled(Box)({
 });
 
 export interface NavigatorProps {
-  value: string
-  type: 'side' | 'bottom'
+  value: string;
+  type: 'side' | 'bottom';
+  comparing: boolean;
 }
 
-export function Navigator({ value, type }: NavigatorProps) {
+export function Navigator({ value, type, comparing }: NavigatorProps) {
+  const idx = useMemo(() => {
+    return tabs.findIndex(el => el.id === value)
+  }, [value])
+
   if (type === 'side') {
     return (
       <SideContainer>
         <ColorBox />
-        <div style={{ height: '100%', display: 'flex' }}>
-          <Tabs orientation="vertical" value={value ?? 'overview'}
-                sx={{
-                  '.MuiTabs-flexContainer': {
-                    gap: '16px',
-                  },
-                  '.MuiTab-root': {
-                    fontSize: 12,
-                    textDecoration: 'none',
-                    textTransform: 'none',
-                  },
-                }}
-                variant="scrollable"
-                scrollButtons='auto'
-          >
-            {renderTabs()}
-          </Tabs>
-        </div>
+        <Tabs orientation="vertical" value={value ?? 'overview'}
+              sx={{
+                '.MuiTabs-flexContainer': {
+                  gap: '16px',
+                },
+                '.MuiTab-root': {
+                  fontSize: 14,
+                  textDecoration: 'none',
+                  textTransform: 'none',
+                  py: 0.5,
+                  pl: 4.5,
+                  height: 28,
+                  minHeight: 28,
+                  alignItems: 'flex-start',
+                },
+              }}
+              variant="scrollable"
+              scrollButtons="auto"
+        >
+          {renderTabs(comparing ? 6 : undefined, idx)}
+        </Tabs>
       </SideContainer>
     );
   } else {
     return (
       <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-        <BottomNavigation showLabels value={value ?? 'overview'} >
-          {renderBottomNavigationActions()}
+        <BottomNavigation showLabels value={value ?? 'overview'}>
+          {renderBottomNavigationActions(comparing ? 6 : undefined)}
         </BottomNavigation>
       </Paper>
-    )
+    );
   }
 }
 
-const tabs: { id: string, label: string, icon: JSX.Element }[] = [
-  { id: 'overview', label: 'Overview', icon: <HomeRoundedIcon /> },
-  { id: 'people', label: 'People', icon: <PersonIcon /> },
-  { id: 'commits', label: 'Commits', icon: <GitCommitIcon /> },
-  { id: 'pull-requests', label: 'Pull Requests', icon: <GitPullRequestIcon /> },
-  { id: 'issues', label: 'Issues', icon: <IssueClosedIcon /> },
+const tabs: { id: string, label: string, icon?: JSX.Element }[] = [
+  { id: 'divider-0', label: 'Analytics', icon: <AnalyticsIcon fontSize='inherit' sx={{ mr: 0.5 }} /> },
+  { id: 'overview', label: 'Overview' },
+  { id: 'people', label: 'People' },
+  { id: 'commits', label: 'Commits' },
+  { id: 'pull-requests', label: 'Pull Requests' },
+  { id: 'issues', label: 'Issues' },
+  { id: 'divider-1', label: 'Monthly Stats', icon: <AutoGraphIcon fontSize='inherit' sx={{ mr: 0.5 }} /> },
+  { id: 'contributors', label: 'Contributors' },
 ];
 
-const renderTabs = () => {
-  return tabs.map(tab => (
-    <Tab
-      key={tab.id}
-      label={tab.label}
-      value={tab.id}
-      icon={tab.icon}
-      disableRipple
-      onClick={useEventCallback(() => {
-        document.getElementById(tab.id)?.scrollIntoView();
-      })}
-    />
-  ))
+const matched = (n: number, i: number) => {
+  return i > n && i - n <= 5
 }
 
-const renderBottomNavigationActions = () => {
-  return tabs.map(tab => (
-    <BottomNavigationAction
-      key={tab.id}
-      label={tab.label}
-      value={tab.id}
-      icon={tab.icon}
-      disableRipple
-      onClick={useEventCallback(() => {
-        document.getElementById(tab.id)?.scrollIntoView();
-      })}
-    />
-  ))
-}
+const renderTabs = (n: number | undefined, index: number) => {
+  return tabs.slice(0, n).map((tab, i) => {
+    if (tab.id.startsWith('divider-')) {
+      return (
+        <Stack sx={{ fontSize: 16, fontWeight: 'bold', pl: 2, color: matched(i, index) ? 'primary.main' : undefined }} direction="row" alignItems='center'>
+          {tab.icon}
+          <span>
+            {tab.label}
+          </span>
+        </Stack>
+      );
+    } else {
+      return (
+        <Tab
+          key={tab.id}
+          label={tab.label}
+          value={tab.id}
+          icon={tab.icon}
+          disableRipple
+          onClick={useEventCallback(() => {
+            document.getElementById(tab.id)?.scrollIntoView();
+          })}
+        />
+      );
+    }
+  });
+};
+
+const renderBottomNavigationActions = (n?: number) => {
+  return tabs.slice(0, n).map(tab => {
+    if (tab.id.startsWith('divider-')) {
+      return <></>;
+    } else {
+      return (
+        <BottomNavigationAction
+          key={tab.id}
+          label={tab.label}
+          value={tab.id}
+          icon={tab.icon}
+          disableRipple
+          onClick={useEventCallback(() => {
+            document.getElementById(tab.id)?.scrollIntoView();
+          })}
+        />
+      );
+    }
+  });
+};
