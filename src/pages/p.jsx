d6335cfc06730a306919c89de1dfa525f5ef78e7
sykp241095 <sykp241095@gmail.com>
2022-06-22 10:59:40 +0800

    urls: Add more short urls (#436)
    Add more short urls

---
+++ src/pages/p.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Redirect} from '@docusaurus/router';
+
+export default function ShortP() {
+  return <Redirect to="/?utm_source=LinkedIn&utm_medium=promotion&utm_campaign=database_pic" />;
+};
