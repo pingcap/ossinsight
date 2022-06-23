934295637ca76a2e3e8bbabcd8786d63f8e01cec
ChenlingLu <55385323+ChenlingLu@users.noreply.github.com>
2022-06-23 23:58:02 +0800

    change the section title

--- src/dynamic-pages/analyze/sections/5-Contributors.tsx
+++ src/dynamic-pages/analyze/sections/5-Contributors.tsx
@@ -252,15 +252,15 @@ export const Contributors = forwardRef(function ({}, ref: ForwardedRef<HTMLEleme
       exclusive
       onChange={handleChangeType}
     >
-      <ToggleButton value='count'>Count</ToggleButton>
-      <ToggleButton value='percentage'>Percentage</ToggleButton>
+      <ToggleButton value='count'># Total Events</ToggleButton>
+      <ToggleButton value='percentage'>% Percentage</ToggleButton>
     </ToggleButtonGroup>
   )
 
   return (
     <Section id="contributors" ref={ref}>
-      <H2>Contributors</H2>
-      <P2>Check the activity of contributors in the repository here, including push and commit events, issue open/close/comment events, code review comments/PRs/submits. 
+      <H2>Contributor Rankings - 2022/05</H2>
+      <P2>Check the activity of contributors in the repository 【 LAST MONTH 】 here, including push and commit events, issue open/close/comment events, code review comments/PRs/submits. 
       <br />
       ✨ Tips: You can view the ranking of specific event in the filter box with two dimensions: the total number of events and the proportion of events that account for all the same events in the repository.
       <br />
