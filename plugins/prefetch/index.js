5c1c617392065cea771a55d3b47d527b497ce6e7
Jagger <634750802@qq.com>
2022-06-27 10:38:43 +0800

    fix: prefetch

--- plugins/prefetch/index.js
+++ plugins/prefetch/index.js
@@ -8,8 +8,11 @@ module.exports = function (context, options) {
       let res = {}
       for (const [key, value] of Object.entries(options)) {
         if (key === 'id') continue;
-        const data = await fs.readFile(value, {encoding: 'utf-8'})
-        res[key] = JSON.parse(data)
+        try {
+          const data = await fs.readFile(value, {encoding: 'utf-8'})
+          res[key] = JSON.parse(data)
+        } catch (e) {
+        }
       }
       setGlobalData(res)
     },
