3b5b8106b518beed8baa32586e93e37ecedb0621
Jagger <634750802@qq.com>
2022-06-27 09:50:44 +0800

    fix: contributor dimension selector content (#479)

--- src/dynamic-pages/analyze/sections/5-Contributors.tsx
+++ src/dynamic-pages/analyze/sections/5-Contributors.tsx
@@ -231,7 +231,6 @@ export const Contributors = forwardRef(function ({}, ref: ForwardedRef<HTMLEleme
         value={descriptor.key}
         onChange={handleChangeDescriptor}
         native={false}
-        renderValue={descriptor => descriptor.title}
       >
         {descriptors.map(descriptor => (
           <MenuItem key={descriptor.key} value={descriptor.key}>{descriptor.title}</MenuItem>
