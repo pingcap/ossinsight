c3a6f323fab869f93919606181133c27f3a61926
Hooopo Wang <hoooopo@gmail.com>
2022-06-28 20:04:00 +0800

    use seed

--- backend/start.sh
+++ backend/start.sh
@@ -5,7 +5,6 @@ set -x
 bundle install
 bundle exec rails db:create
 bundle exec rails db:migrate
-bundle exec rake gh:load_sample
-bundle exec rake gh:load_collection
+bundle exec rake  db:seed
 bundle exec rake gh:set_tiflash_replica
 bundle exec rails runner 'Realtime.new(ENV["GITHUB_TOKEN"].to_s.split(","), 100).run'
