fc024c470c2eb0d193f19703213cb687f09d8bfa
Mini256 <minianter@foxmail.com>
2022-07-04 14:29:18 +0800

    fix: get-user-by-login skip the null actor id

--- api/queries/get-user-by-login/template.sql
+++ api/queries/get-user-by-login/template.sql
@@ -1,1 +1,1 @@
-select actor_id AS id, 'Mini256' AS login from github_events where actor_login = 'Mini256' limit 1;
+select actor_id AS id, 'Mini256' AS login from github_events where actor_login = 'Mini256' AND actor_id IS NOT NULL limit 1;
