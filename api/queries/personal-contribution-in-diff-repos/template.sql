54edeb8aa404e522cf5eca97cbb1adedc2b6989b
Mini256 <minianter@foxmail.com>
2022-07-05 16:19:50 +0800

    query: support select period on personal contribution in diff repos query (#521)

--- api/queries/personal-contribution-in-diff-repos/template.sql
+++ api/queries/personal-contribution-in-diff-repos/template.sql
@@ -1,8 +1,12 @@
-SELECT repo_id, ANY_VALUE(repo_name) AS repo_name, COUNT(DISTINCT id) AS cnt, DATE_FORMAT(created_at, '%Y-%m-%d %k:00:00') AS event_hour
+SELECT
+    repo_id,
+    ANY_VALUE(repo_name) AS repo_name,
+    COUNT(DISTINCT id) AS cnt,
+    DATE_FORMAT(created_at, '%Y-%m-%d %k:00:00') AS event_period
 FROM github_events ge
 WHERE
-    type = 'PullRequestEvent' AND action ='opened'
-    AND actor_id = 5086433
+    actor_id = 5086433
+    AND type = 'PullRequestEvent' AND action ='opened'
     AND (created_at BETWEEN DATE_SUB(NOW(), INTERVAL 7 DAY) AND NOW())
-GROUP BY repo_id, event_hour
-ORDER BY repo_id, event_hour DESC
+GROUP BY repo_id, event_period
+ORDER BY repo_id, event_period DESC
