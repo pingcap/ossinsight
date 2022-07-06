dcfde68860c0c6288cd356f08247a23fc6e9d978
Mini256 <minianter@foxmail.com>
2022-07-06 11:33:06 +0800

    fix: unify contributions

--- api/queries/personal-contribution-time-distribution/template.sql
+++ api/queries/personal-contribution-time-distribution/template.sql
@@ -2,11 +2,25 @@ SELECT
     DAYOFWEEK(created_at) - 1 AS dayofweek,
     HOUR(created_at) AS hour,
     COUNT(*) AS cnt,
-    type
+    CASE type
+        WHEN 'IssuesEvent' THEN 'issues'
+        WHEN 'IssueCommentEvent' THEN 'issue_comments'
+        WHEN 'PullRequestEvent' THEN 'pull_requests'
+        WHEN 'PullRequestReviewEvent' THEN 'reviews'
+        WHEN 'PullRequestReviewCommentEvent' THEN 'review_comments'
+        WHEN 'PushEvent' THEN 'pushes'
+    END AS type,
 FROM github_events ge
 WHERE
     actor_id = 5086433
-    AND type IN ('PushEvent', 'PullRequestEvent', 'PullRequestReviewEvent', 'PullRequestReviewCommentEvent', 'IssuesEvent', 'IssueCommentEvent')
+    AND (
+        (type = 'PullRequestEvent' AND action = 'opened') OR
+        (type = 'IssuesEvent' AND action = 'opened') OR
+        (type = 'IssueCommentEvent' AND action = 'created') OR
+        (type = 'PullRequestReviewEvent' AND action = 'created') OR
+        (type = 'PullRequestReviewCommentEvent' AND action = 'created') OR
+        (type = 'PushEvent')
+    )
 GROUP BY dayofweek, hour, type
 ORDER BY dayofweek, hour, type
 ;
