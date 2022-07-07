8fbd39c4e45f897af4e14821a9b104e6d61f7328
Mini256 <minianter@foxmail.com>
2022-07-07 14:52:22 +0800

    fix: get user id by login should get the latest one (#536)

--- api/queries/get-user-by-login/template.sql
+++ api/queries/get-user-by-login/template.sql
@@ -1,1 +1,8 @@
-select actor_id AS id, 'Mini256' AS login from github_events where actor_login = 'Mini256' AND actor_id IS NOT NULL limit 1;
+SELECT
+    actor_id AS id, 'Mini256' AS login
+FROM github_events
+WHERE
+    actor_login = 'Mini256'
+    AND actor_id IS NOT NULL
+ORDER BY created_at DESC
+LIMIT 1;
