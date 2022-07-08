1212907338c9b4e6373e0fc60b4079ff84e3f3e6
Jagger <634750802@qq.com>
2022-07-08 16:16:04 +0800

    feat: company analyze page (beta) (#546)
    * first impl
    
    * styling
    
    * Update index.tsx
    
    * styling
    
    * Update index.tsx
    
    Co-authored-by: sykp241095 <sykp241095@gmail.com>

---
+++ src/pages/analyze-company/_components/SearchCompany.tsx
@@ -0,0 +1,76 @@
+import Autocomplete from "@mui/material/Autocomplete";
+import React, { ChangeEventHandler, useMemo, useState } from "react";
+import TextField from "@mui/material/TextField";
+import { CompanyInfo, useCompanyList } from "./hooks";
+import { ListItem, ListItemButton, ListItemText, Popper, useEventCallback } from "@mui/material";
+import { useDebounced } from "../../../components/CompareHeader/useSearchRepo";
+import InputAdornment from "@mui/material/InputAdornment";
+import { Search } from "@mui/icons-material";
+
+interface SearchCompanyProps {
+  value: CompanyInfo | null;
+  onChange: (value: CompanyInfo | null) => void;
+}
+
+const SearchCompany = ({ value, onChange }: SearchCompanyProps) => {
+  const [keyword, setKeyword] = useState('');
+  const searchKey = useDebounced(keyword, 500);
+  const { data: companies, loading } = useCompanyList(searchKey);
+
+  const handleChange = useEventCallback((_: any, value: CompanyInfo) => {
+    onChange(value);
+  });
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = useEventCallback((e) => {
+    setKeyword(e.target.value);
+  });
+
+  return (
+    <Autocomplete<CompanyInfo, undefined, undefined, false>
+      value={value}
+      onChange={handleChange}
+      id="combo-box-demo"
+      loading={loading}
+      options={companies ?? []}
+      fullWidth
+      openOnFocus
+      getOptionLabel={option => option.name}
+      renderOption={(props, option, state) => (
+        <ListItem {...props} selected={state.selected} >
+          <ListItemText primary={option.name} secondary={`${option.total} Developers`} />
+        </ListItem>
+      )}
+      renderInput={({ InputProps, ...params }) => (
+        <TextField
+          {...params}
+          sx={{
+            backgroundColor: 'white',
+            borderRadius: 2
+          }}
+          onChange={handleInputChange}
+          InputProps={{
+            ...InputProps,
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search htmlColor='#333' />
+              </InputAdornment>
+            ),
+            placeholder: 'Search by companies',
+            sx: {
+              color: '#333',
+              "::placeholder": {
+                color: '#dfdfdf'
+              },
+              borderRadius: 2
+            }
+        }}
+        />
+      )}
+      PopperComponent={CustomPopper}
+    />
+  );
+};
+
+const CustomPopper = props => <Popper {...props} placement='bottom-start' />
+
+export default SearchCompany;
---
+++ src/pages/analyze-company/index.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import CustomPage from "../../theme/CustomPage";
+import SearchCompany from "./_components/SearchCompany";
+import Companies from "./_components/Companies";
+import Container from "@mui/material/Container";
+import { CompanyInfo } from "./_components/hooks";
+import Typography from "@mui/material/Typography";
+
+const Page = () => {
+  const [company, setCompany] = useState<CompanyInfo>(null)
+
+  return (
+    <CustomPage>
+      <Container maxWidth="lg" sx={{ py: 8 }}>
+        <Typography variant='h1' align='center'>🧪 Company Analytics [Beta] </Typography>
+        <Typography variant='body1' align='center' mt={2} mb={6}>
+          Contribution analytics of developers within the same company
+        </Typography>
+        <SearchCompany value={company} onChange={setCompany} />
+        <Typography variant='body2' align='center' mt={2} mb={6}>
+          Hints: Only the statistics of the members who have the data of [company name] was recorded and the result may include all events in both previous/current company name. Contributions include pushes, pull requests, pull request reviews, pull request review comments, issues and issue comments.
+        </Typography>
+        <Companies company={company} />
+      </Container>
+    </CustomPage>
+  )
+}
+
+export default Page
