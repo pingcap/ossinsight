0438e2573c5c2159744b5e453fa06a72065c081d
ChenlingLu <55385323+ChenlingLu@users.noreply.github.com>
2022-07-11 18:01:26 +0800

    delete brs

--- src/pages/home/_sections/1-collections/left/SubtitleLine.tsx
+++ src/pages/home/_sections/1-collections/left/SubtitleLine.tsx
@@ -4,9 +4,7 @@ import { Subtitle } from '../../../_components/typography';
 export default function SubtitleLine () {
   return (
     <Subtitle>
-      Insights about the <b>monthly & historical rankings and trends</b> 
-      <br />
-      in technical fields with curated repository lists.
+      Insights about the <b>monthly & historical rankings and trends</b> in technical fields with curated repository lists.
     </Subtitle>
   )
 }
