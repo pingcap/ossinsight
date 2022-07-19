8eeb96421395a760ba1b6966bb018b396e6e672e
Mini256 <minianter@foxmail.com>
2022-07-19 10:08:17 +0800

    fix: remove star history language

--- api/queries/personal-star-history/template.sql
+++ api/queries/personal-star-history/template.sql
@@ -10,18 +10,19 @@ WITH star_repos AS (
             actor_id = 5086433 AND type = 'WatchEvent'
     ) sub
     GROUP BY repo_id
-), repo_with_language AS (
-    SELECT repo_id, ANY_VALUE(language) AS language
-    FROM (
-        SELECT
-            ge.repo_id,
-            FIRST_VALUE(language) OVER (PARTITION BY ge.repo_id ORDER BY created_at DESC) AS language
-        FROM github_events ge
-        JOIN star_repos sr ON ge.repo_id = sr.repo_id
-        WHERE
-            type = 'PullRequestEvent'
-   ) sub
-   GROUP BY repo_id 
+-- )
+-- , repo_with_language AS (
+--     SELECT repo_id, ANY_VALUE(language) AS language
+--     FROM (
+--         SELECT
+--             ge.repo_id,
+--             FIRST_VALUE(language) OVER (PARTITION BY ge.repo_id ORDER BY created_at DESC) AS language
+--         FROM github_events ge
+--         JOIN star_repos sr ON ge.repo_id = sr.repo_id
+--         WHERE
+--             type = 'PullRequestEvent'
+--    ) sub
+--    GROUP BY repo_id 
 ), star_repos_count_by_month AS (
     SELECT
         sr.star_month,
@@ -32,11 +33,12 @@ WITH star_repos AS (
 )
 SELECT
     sr.star_month,
-    IFNULL(rl.language, 'Unknown') AS language,
+    -- IFNULL(rl.language, 'Unknown') AS language,
+    'All' AS language,
     COUNT(sr.repo_id) AS cnt,
     COUNT(sr.repo_id) / m.month_cnt AS percentage
 FROM star_repos sr
 JOIN star_repos_count_by_month m ON sr.star_month = m.star_month
-LEFT JOIN repo_with_language rl ON sr.repo_id = rl.repo_id
+-- LEFT JOIN repo_with_language rl ON sr.repo_id = rl.repo_id
 GROUP BY star_month, language
 ORDER BY star_month, language;
