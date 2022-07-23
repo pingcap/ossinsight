86b04b30660dd6c532050b6e15b555ac81c90c50
sykp241095 <sykp241095@gmail.com>
2022-07-23 22:33:50 +0800

    Delete d.jsx

--- src/pages/d.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import {Redirect} from '@docusaurus/router';
-
-export default function ShortD() {
-  return <Redirect to="/?utm_source=LinkedIn&utm_medium=promotion&utm_campaign=database" />;
-};
