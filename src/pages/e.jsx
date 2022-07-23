66d3f6eaa77780be18134e56e62c49d42d801ce5
sykp241095 <sykp241095@gmail.com>
2022-07-23 22:33:40 +0800

    Delete e.jsx

--- src/pages/e.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import {Redirect} from '@docusaurus/router';
-
-export default function ShortE() {
-  return <Redirect to="/collections/open-source-database/?utm_source=LinkedIn&utm_medium=promotion&utm_campaign=explore" />;
-};
