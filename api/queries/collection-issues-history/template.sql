bccb6b4ce85371f69a78a1ac4e6e07e6bbd2abb0
Mini256 <minianter@foxmail.com>
2022-08-02 17:03:11 +0800

    fix: use number instead of pr_or_issue_number for distinct

--- api/queries/collection-issues-history/template.sql
+++ api/queries/collection-issues-history/template.sql
@@ -1,9 +1,9 @@
 WITH issues_with_latest_repo_name AS (
     SELECT
         event_month,
-        pr_or_issue_id,
+        number,
         FIRST_VALUE(repo_name) OVER (PARTITION BY repo_id ORDER BY created_at DESC) AS repo_name,
-        ROW_NUMBER() OVER(PARTITION BY pr_or_issue_id) AS row_num
+        ROW_NUMBER() OVER(PARTITION BY number) AS row_num
     FROM github_events
     WHERE
         type = 'IssuesEvent' AND repo_id IN (41986369, 16563587, 105944401)
@@ -14,7 +14,7 @@ WITH issues_with_latest_repo_name AS (
     SELECT
         event_month,
         repo_name,
-        COUNT(pr_or_issue_id) OVER(PARTITION BY repo_name ORDER BY event_month ASC) AS total
+        COUNT(number) OVER(PARTITION BY repo_name ORDER BY event_month ASC) AS total
     FROM issues_with_latest_repo_name
     WHERE row_num = 1
     ORDER BY 1
