773276fd03888d297146da20af9192c83bab785c
Hooopo Wang <hoooopo@gmail.com>
2022-08-02 14:18:52 +0800

    fix repeated started events

--- backend/lib/trending_repo_generator.rb
+++ backend/lib/trending_repo_generator.rb
@@ -6,12 +6,12 @@ class TrendingRepoGenerator
 
   def generate
     sql = <<~SQL
-      select repo_name, count(*) as count
+      select repo_name, count(distinct actor_login) as count
       from github_events
       where type = 'WatchEvent' and created_at >= '#{7.days.ago.to_s(:db)}'
             and repo_name not in (select repo_name from trending_repos where repo_name is not null)
       group by 1
-      having count(*) >= 500
+      having count(distinct actor_login) >= 500
       order by 2 desc
       limit #{limit}
     SQL
