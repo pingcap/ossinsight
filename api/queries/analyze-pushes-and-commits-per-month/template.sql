893eecc7ad84986449ead17187be108392bfbc51
Mini256 <minianter@foxmail.com>
2022-08-05 13:46:03 +0800

    fix: simpify sqls

--- api/queries/analyze-pushes-and-commits-per-month/template.sql
+++ api/queries/analyze-pushes-and-commits-per-month/template.sql
@@ -1,8 +1,8 @@
-select
+SELECT
     event_month,
-    ifnull(count(*), 0) as pushes,
-    sum(coalesce(push_distinct_size, push_size, 0)) as commits
-from github_events
-where repo_id = 41986369 and type = 'PushEvent'
-group by event_month
-order by event_month;
+    IFNULL(COUNT(*), 0) AS pushes,
+    SUM(COALESCE(push_distinct_size, push_size, 0)) AS commits
+FROM github_events
+WHERE repo_id = 41986369 and type = 'PushEvent'
+GROUP BY event_month
+ORDER BY event_month;
--- api/queries/stars-history/template.sql
+++ api/queries/stars-history/template.sql
@@ -1,18 +1,18 @@
-WITH prs_with_latest_repo_name AS (
-    SELECT
-        event_month,
-        actor_login,
-        FIRST_VALUE(repo_name) OVER (PARTITION BY repo_id ORDER BY created_at DESC) AS repo_name,
-        ROW_NUMBER() OVER(PARTITION BY actor_login) AS row_num
-    FROM github_events
-    WHERE
-        type = 'WatchEvent' AND repo_id = 41986369
-), acc AS (
+WITH acc AS (
     SELECT
         event_month,
         repo_name,
         COUNT(actor_login) OVER(PARTITION BY repo_name ORDER BY event_month ASC) AS total
-    FROM prs_with_latest_repo_name
+    FROM (
+        SELECT
+            event_month,
+            actor_login,
+            FIRST_VALUE(repo_name) OVER (PARTITION BY repo_id ORDER BY created_at DESC) AS repo_name,
+            ROW_NUMBER() OVER(PARTITION BY actor_login) AS row_num
+        FROM github_events
+        WHERE
+            type = 'WatchEvent' AND repo_id = 41986369
+    ) prs_with_latest_repo_name
     WHERE row_num = 1
     ORDER BY 1
 )
