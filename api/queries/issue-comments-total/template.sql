725a05f5f470fb2789c3165abaece3879d5802a6
Mini256 <minianter@foxmail.com>
2022-08-08 14:21:37 +0800

    fix(sql): simpify sqls

--- api/queries/issue-comments-total/template.sql
+++ api/queries/issue-comments-total/template.sql
@@ -1,6 +1,7 @@
 SELECT
-    IFNULL(COUNT(DISTINCT comment_id), 0) AS comments
+    IFNULL(COUNT(1), 0) AS comments
 FROM github_events
 WHERE
     type = 'IssueCommentEvent'
-    AND repo_id = 41986369;
+    AND repo_id = 41986369
+;
