a40ff9672497abe1fe48a409a369c9e4aa256387
Mini256 <minianter@foxmail.com>
2022-08-16 21:16:17 +0800

    fix: fix personal pr size query

--- api/queries/personal-pull-request-size-history/template.sql
+++ api/queries/personal-pull-request-size-history/template.sql
@@ -1,17 +1,21 @@
-SELECT
-    event_month,
-    SUM(CASE WHEN (additions + deletions) < 10 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS xs,
-    SUM(CASE WHEN (additions + deletions) >= 10 AND (additions + deletions) < 30 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS s,
-    SUM(CASE WHEN (additions + deletions) >= 30 AND (additions + deletions) < 100 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS m,
-    SUM(CASE WHEN (additions + deletions) >= 100 AND (additions + deletions) < 500 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS l,
-    SUM(CASE WHEN (additions + deletions) >= 500 AND (additions + deletions) < 1000 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS xl,
-    SUM(CASE WHEN (additions + deletions) >= 1000 THEN 1 ELSE 0 END) OVER (PARTITION BY event_month) AS xxl,
-    COUNT(*) OVER (PARTITION BY event_month) AS all_size
-FROM github_events ge
+SELECT event_month, xs, s, m, l, xl, all_size
+FROM (
+    SELECT
+        event_month,
+        SUM(CASE WHEN (additions + deletions) < 10 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS xs,
+        SUM(CASE WHEN (additions + deletions) >= 10 AND (additions + deletions) < 30 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS s,
+        SUM(CASE WHEN (additions + deletions) >= 30 AND (additions + deletions) < 100 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS m,
+        SUM(CASE WHEN (additions + deletions) >= 100 AND (additions + deletions) < 500 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS l,
+        SUM(CASE WHEN (additions + deletions) >= 500 AND (additions + deletions) < 1000 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS xl,
+        SUM(CASE WHEN (additions + deletions) >= 1000 THEN 1 ELSE 0 END) OVER (PARTITION BY event_month) AS xxl,
+        COUNT(*) OVER (PARTITION BY event_month) AS all_size,
+        ROW_NUMBER() OVER (PARTITION BY event_month) AS row_num
+    FROM github_events ge
+    WHERE
+        creator_user_id = 5086433
+        AND type = 'PullRequestEvent'
+        AND action = 'closed'
+        AND pr_merged = true
+) AS sub
 WHERE
-    creator_user_id = 5086433
-    AND type = 'PullRequestEvent'
-    AND action = 'closed'
-    AND pr_merged = true
-GROUP BY event_month
-ORDER BY event_month
+    row_num = 1
