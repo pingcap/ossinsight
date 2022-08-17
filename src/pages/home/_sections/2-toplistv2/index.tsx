7cd6267e801d916b14526f2955401c05e5f1f774
sykp241095 <sykp241095@gmail.com>
2022-08-17 17:05:24 +0800

    Update index.tsx

--- src/pages/home/_sections/2-toplistv2/index.tsx
+++ src/pages/home/_sections/2-toplistv2/index.tsx
@@ -8,7 +8,7 @@ export function TopListV2Section() {
   return (
     <Section maxWidth={false}>
       <a href="#trending-repos">
-      <H2 id="trending-repos" sx={{ fontSize: 24, mb: 2, color:'white' }} >🔥 Trending Repo</H2></a>
+      <H2 id="trending-repos" sx={{ fontSize: 24, mb: 2, color:'white' }} >🔥 Trending Repos</H2></a>
       <Body sx={{ mb: 4, mt: 2, fontSize: 14 }}>Active repositories ranked by star numbers. Query was filtered due to massive bots’ commits. </Body>
       <TopListV2 />
     </Section>
