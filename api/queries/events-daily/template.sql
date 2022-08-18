c2efa81022b6272cc254c20aaf2f70668d5c24be
Mini256 <minianter@foxmail.com>
2022-08-18 20:47:52 +0800

    fix(sql): fix events-daily

--- api/queries/events-daily/template.sql
+++ api/queries/events-daily/template.sql
@@ -2,6 +2,6 @@ SELECT event_day, COUNT(*) AS events
 FROM github_events ge
 WHERE
     type = 'PullRequestEvent'
-    AND event_day = CURRENT_DATE()
+    AND event_year = YEAR(NOW())
 GROUP BY event_day
 ORDER BY event_day;
