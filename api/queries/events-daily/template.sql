e7f7748f9b1e6bf2f14912ad6ad921053d2bf60b
Mini256 <minianter@foxmail.com>
2022-08-18 19:59:05 +0800

    fix(sql): set event daily query refresh hour to 0.16

--- api/queries/events-daily/template.sql
+++ api/queries/events-daily/template.sql
@@ -1,6 +1,7 @@
 SELECT event_day, COUNT(*) AS events
 FROM github_events ge
 WHERE
-    event_year = YEAR(NOW())
+    type = 'PullRequestEvent'
+    AND event_day = CURRENT_DATE()
 GROUP BY event_day
 ORDER BY event_day;
