986267669af66db7ec1acfb74380956586ee4655
Hooopo <Hoooopo@gmail.com>
2022-08-18 16:04:31 +0800

    Update seeds.rb

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -3,5 +3,4 @@ return if Collection.count > 0
 seed_file = Rails.root.join("db", "seed.sql").to_s
 uri = URI.parse(ENV['DATABASE_URL'])
 cmd = "mysql -u #{uri.user} --port=#{uri.port} --password=#{uri.password} -h #{uri.host} #{uri.path.sub('/', '')} < #{seed_file} "
-puts cmd
 system(cmd)
