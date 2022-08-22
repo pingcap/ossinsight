602dc6f812d1e54a4c71d0d8eb47a387e58353f6
sykp241095 <sykp241095@gmail.com>
2022-08-22 14:52:25 +0800

    Use created_at instead of event_month

--- api/queries/commits-time-distribution/template.sql
+++ api/queries/commits-time-distribution/template.sql
@@ -7,7 +7,7 @@ WHERE
     repo_id = 41986369
     AND type = 'PushEvent'
     AND action IS NULL
-    AND (event_month BETWEEN DATE_FORMAT(DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR), '%Y-%m-01') AND DATE_FORMAT(CURRENT_DATE(), '%Y-%m-01'))
+    AND (DATE_FORMAT(created_at, '%Y-%m-01') BETWEEN DATE_FORMAT(DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR), '%Y-%m-01') AND DATE_FORMAT(CURRENT_DATE(), '%Y-%m-01'))
 GROUP BY dayofweek, hour
 ORDER BY dayofweek, hour
 ;
