89894dd88d99f0f140b4246139b01c117375e2f8
sykp241095 <sykp241095@gmail.com>
2022-08-24 11:49:21 +0800

    sql: use `created_at` instead of `event_month` (#734)
    * Use created_at instead of event_month
    
    * Use created_at instead of event_month
    
    * Use created_at instead of event_month
    
    * Use created_at directly
    
    * fix
    
    Co-authored-by: Mini256 <minianter@foxmail.com>

--- api/queries/commits-time-distribution/template.sql
+++ api/queries/commits-time-distribution/template.sql
@@ -7,7 +7,7 @@ WHERE
     repo_id = 41986369
     AND type = 'PushEvent'
     AND action IS NULL
-    AND (event_month BETWEEN DATE_FORMAT(DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR), '%Y-%m-01') AND DATE_FORMAT(CURRENT_DATE(), '%Y-%m-01'))
+    AND (created_at BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR) AND CURRENT_DATE())
 GROUP BY dayofweek, hour
 ORDER BY dayofweek, hour
 ;
