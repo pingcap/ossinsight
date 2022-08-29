a7753db60d32263d615dfea6e82dfbbe7cfe03ea
Mini256 <minianter@foxmail.com>
2022-08-29 12:35:40 +0800

    fix: refine sql

--- api/queries/personal-pull-request-size-history/template.sql
+++ api/queries/personal-pull-request-size-history/template.sql
@@ -1,21 +1,26 @@
 SELECT event_month, xs, s, m, l, xl, all_size
 FROM (
     SELECT
-        DATE_FORMAT(created_at, '%Y-%m-01') AS event_month,
-        SUM(CASE WHEN (additions + deletions) < 10 THEN 1 ELSE 0 END) OVER(PARTITION BY DATE_FORMAT(created_at, '%Y-%m-01')) AS xs,
-        SUM(CASE WHEN (additions + deletions) >= 10 AND (additions + deletions) < 30 THEN 1 ELSE 0 END) OVER(PARTITION BY DATE_FORMAT(created_at, '%Y-%m-01')) AS s,
-        SUM(CASE WHEN (additions + deletions) >= 30 AND (additions + deletions) < 100 THEN 1 ELSE 0 END) OVER(PARTITION BY DATE_FORMAT(created_at, '%Y-%m-01')) AS m,
-        SUM(CASE WHEN (additions + deletions) >= 100 AND (additions + deletions) < 500 THEN 1 ELSE 0 END) OVER(PARTITION BY DATE_FORMAT(created_at, '%Y-%m-01')) AS l,
-        SUM(CASE WHEN (additions + deletions) >= 500 AND (additions + deletions) < 1000 THEN 1 ELSE 0 END) OVER(PARTITION BY DATE_FORMAT(created_at, '%Y-%m-01')) AS xl,
-        SUM(CASE WHEN (additions + deletions) >= 1000 THEN 1 ELSE 0 END) OVER (PARTITION BY DATE_FORMAT(created_at, '%Y-%m-01')) AS xxl,
-        COUNT(*) OVER (PARTITION BY DATE_FORMAT(created_at, '%Y-%m-01')) AS all_size,
-        ROW_NUMBER() OVER (PARTITION BY DATE_FORMAT(created_at, '%Y-%m-01')) AS row_num
-    FROM github_events ge
-    WHERE
-        creator_user_id = 5086433
-        AND type = 'PullRequestEvent'
-        AND action = 'closed'
-        AND pr_merged = true
+        event_month,
+        SUM(CASE WHEN changes < 10 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS xs,
+        SUM(CASE WHEN changes >= 10 AND changes < 30 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS s,
+        SUM(CASE WHEN changes >= 30 AND changes < 100 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS m,
+        SUM(CASE WHEN changes >= 100 AND changes < 500 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS l,
+        SUM(CASE WHEN changes >= 500 AND changes < 1000 THEN 1 ELSE 0 END) OVER(PARTITION BY event_month) AS xl,
+        SUM(CASE WHEN changes >= 1000 THEN 1 ELSE 0 END) OVER (PARTITION BY event_month) AS xxl,
+        COUNT(*) OVER (PARTITION BY event_month) AS all_size,
+        ROW_NUMBER() OVER (PARTITION BY event_month) AS row_num
+    FROM (
+        SELECT
+            DATE_FORMAT(created_at, '%Y-%m-01') AS event_month,
+            additions + deletions AS changes
+        FROM github_events ge
+        WHERE
+            creator_user_id = 5086433
+            AND type = 'PullRequestEvent'
+            AND action = 'closed'
+            AND pr_merged = true
+    ) sub
 ) AS sub
 WHERE
     row_num = 1
