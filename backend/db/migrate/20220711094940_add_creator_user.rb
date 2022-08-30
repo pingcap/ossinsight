7654d245dab67b60cf6e5eee2ca6c4bbda47a6ba
Hooopo <Hoooopo@gmail.com>
2022-08-30 16:29:23 +0800

    Update 20220711094940_add_creator_user.rb

--- backend/db/migrate/20220711094940_add_creator_user.rb
+++ backend/db/migrate/20220711094940_add_creator_user.rb
@@ -1,7 +1,7 @@
 class AddCreatorUser < ActiveRecord::Migration[6.1]
   def change
-    add_column :github_events, :creator_user_login, :string 
-    add_column :github_events, :creator_user_id, :bigint
-    add_column :github_events, :pr_or_issue_created_at, :datetime
+    add_column :github_events, :creator_user_login, :string, if_not_exists: true
+    add_column :github_events, :creator_user_id, :bigint, if_not_exists: true
+    add_column :github_events, :pr_or_issue_created_at, :datetime, if_not_exists: true
   end
 end
