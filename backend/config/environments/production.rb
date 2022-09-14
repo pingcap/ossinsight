60505e7a42f86e1c9b318ec86a1d1e812fef66cc
Hooopo Wang <hoooopo@gmail.com>
2022-09-14 17:38:39 +0800

    log level error

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = :info
+  config.log_level = :error
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
