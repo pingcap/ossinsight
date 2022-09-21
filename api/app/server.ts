262287488f6f4b1dcc884f4bed82774bb75624ee
Jagger <634750802@qq.com>
2022-09-21 11:39:16 +0800

    feat: new ws query entrypoint

--- api/app/server.ts
+++ api/app/server.ts
@@ -12,6 +12,7 @@ import GhExecutor from "./core/GhExecutor";
 import CollectionService from "./services/CollectionService";
 import UserService from "./services/UserService";
 import GHEventService from "./services/GHEventService";
+import { CachedData } from "./core/cache/Cache";
 
 export default async function httpServerRoutes(
   router: Router<DefaultState, ContextExtends>,
@@ -160,9 +161,11 @@ export default async function httpServerRoutes(
 }
 
 export function socketServerRoutes(
-  socket: Socket, io: Server, logger: Consola, queryExecutor: TiDBQueryExecutor, cacheBuilder: CacheBuilder, 
+  socket: Socket, io: Server, logger: Consola, queryExecutor: TiDBQueryExecutor, cacheBuilder: CacheBuilder,
   collectionService: CollectionService, userService: UserService, ghEventService: GHEventService
 ) {
+  // Deprecated: use 'q' below instead.
+  //
   // queryMsg example 1: events-increment?ts=1662519722
   // queryMsg example 2: events-increment-list
   // queryMsg example 3: events-total
@@ -198,6 +201,78 @@ export function socketServerRoutes(
       socket.emit(queryType, res);
     } catch (error) {
       logger.error("Failed to request %s[ws]: ", queryMsg, error);
+    }
+  });
+
+  /*
+   * This ws entrypoint provide a method to visit HTTP /q/:query and /q/explain/:query equally.
+   * Client side should send a json message "{ qid?, explain, query, params }" to request a query.
+   *
+   * - Param `qid`: If `qid` exists, server will emit response into '/q/{query}?qid={qid}' topic. Otherwise,
+   * server will emit response directly to '/q/{query}' which is reusable across different
+   * subscribers.
+   *
+   * - Param `explain`: If `explain` exists, server will execute '/q/explain/{query}' instead, and to response topic
+   * would be `/q/explain/{query}?qid={qid}`
+   *
+   * - Error handling: If error occurs in Query.run phase, response.error would set to true, and payload
+   * will be the error data.
+   */
+  socket.on("q", async (queryMsg: string) => {
+    interface WsQueryRequest {
+      qid?: string | number
+      explain?: boolean
+      query: string
+      params: Record<string, any>
+    }
+
+    interface WsQueryResponse {
+      qid?: string | number
+      explain?: boolean
+      error?: true
+      payload: any
+    }
+
+    try {
+      const request: WsQueryRequest = JSON.parse(queryMsg)
+      const topic = `/q/${request.explain ? 'explain/' : ''}${request.query}${request.qid ? `?qid=${request.qid}` : ''}`
+      let response: WsQueryResponse
+
+      try {
+        const q = new Query(
+          request.query,
+          cacheBuilder,
+          queryExecutor,
+          ghEventService,
+          collectionService,
+          userService
+        );
+        let res
+        if (request.explain) {
+          res = await q.explain(request.params);
+        } else {
+          res = await q.run(request.params, false, null, socket.handshake.address, true);
+        }
+        response = {
+          qid: request.qid,
+          explain: request.explain,
+          payload: res
+        }
+      } catch (e) {
+        response = {
+          error: true,
+          qid: request.qid,
+          explain: request.explain,
+          payload: e
+        }
+      }
+      socket.emit(topic, response);
+    } catch (error) {
+      logger.error("Failed to request %s[ws]: ", queryMsg, error);
+      socket.emit('fatal-error/q', {
+        request: queryMsg,
+        error,
+      });
     }
   });
 }
