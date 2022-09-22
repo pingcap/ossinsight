c28aafc0dea1a5f289e31e8a341ff004d9b23072
sykp241095 <sykp241095@gmail.com>
2022-09-22 17:57:18 +0800

    Move API to l1 menus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -222,6 +222,7 @@ const config = {
               {label: '└─ Cryptocurrency Insight - not ready', to: '/workshop/cryptocurrency-insight'},
             ]
           },
+          {to: '/docs', label: 'API', position: 'left'},
           /*
           {
             type: 'dropdown',
@@ -240,11 +241,10 @@ const config = {
               {label: 'About OSS Insight', to: '/about'},
               {label: 'About TiDB Cloud', to: 'https://en.pingcap.com/tidb-cloud?utm_source=ossinsight&utm_medium=referral'},
               {label: 'How do we implement OSS Insight?', to: '/blog/why-we-choose-tidb-to-support-ossinsight'},
-              {label: 'API', to: '/docs'},
+              {label: 'Stats', to: '/stats'},
               {label: 'Report an Issue', to: 'https://github.com/pingcap/ossinsight/issues'},
             ]
           },
-          {to: '/stats', label: 'Stats', position: 'left'},
           {
             href: 'https://twitter.com/OSSInsight',
             className: 'navbar-item-twitter',
