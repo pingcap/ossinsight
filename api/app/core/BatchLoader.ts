6202f045052b78e2ac8aa8d281ae90cc4164d044
Mini256 <minianter@foxmail.com>
2022-09-23 09:08:54 +0800

    refactor: refine batch loader

--- api/app/core/BatchLoader.ts
+++ api/app/core/BatchLoader.ts
@@ -1,35 +1,66 @@
-import consola, { Consola } from "consola";
+import consola from "consola";
 import { ConnectionWrapper } from "../utils/db";
 
-// The batch insert size.
-const DEFAULT_BATCH_SIZE = 100;
+export const DEFAULT_BATCH_SIZE = 100;
+export const DEFAULT_FLUSH_INTERVAL = 10;
+export const DEFAULT_MAX_RETRIES = 2;
 
-// The flush interval which control how often do it perform a batch insert 
-// if the batch quantity is not reached.
-// Default: 10 second.
-const DEFAULT_FLUSH_INTERVAL = 10;
+export interface Option {
+    /**
+     * The maxRetries determine how many times the insert statement will be executed
+     * when it failed.
+     * 
+     * Default: 2
+     */
+    maxRetries?: number;
+    /**
+     * The flushInterval is used to control how often do it perform a batch insert
+     * if the batch size is not reached. If it is less than 0, will not flush intermittently.
+     * 
+     * Default: 10 second.
+     */
+    flushInterval?: number;
+    /**
+     * The batchSize is used to control how many records will be insert once.
+     * 
+     * Default: 100
+     */
+    batchSize?: number;
+}
 
 export class BatchLoader {
-    private conn: ConnectionWrapper;
     private buf: any[];
-    private logger: Consola;
-    private timer: NodeJS.Timer;
+    private logger = consola.withTag('batch-loader');
+    private batchSize: number;
+    private maxRetries: number;
 
     constructor(
-        conn: ConnectionWrapper,
+        // TODO: replace with connection promisify.
+        readonly conn: ConnectionWrapper,
         readonly sql: string,
-        readonly batchSize: number = DEFAULT_BATCH_SIZE,
-        readonly flushInterval: number = DEFAULT_FLUSH_INTERVAL
+        readonly option: Option = {}
     ) {
-        this.conn = conn;
         this.buf = [];
-        this.logger = consola.withTag('batch-loader');
-        this.timer = setInterval(async () => {
-            if (this.buf.length !== 0) {
-                this.logger.debug(`Loader flush after ${flushInterval} seconds interval.`);
-                await this.flush();
-            }
-        }, flushInterval * 1000);
+
+        this.maxRetries = option.maxRetries || DEFAULT_MAX_RETRIES;
+        if (this.maxRetries < 0) {
+            this.maxRetries = 0;
+        }
+
+        this.batchSize = option.batchSize || DEFAULT_BATCH_SIZE;
+        if (this.batchSize < 0) {
+            throw new Error('Batch size must greater than 0.');
+        }
+
+        const flushInterval = option.flushInterval || DEFAULT_FLUSH_INTERVAL;
+        if (flushInterval > 0) {
+            setInterval(async () => {
+                if (this.buf.length > 0) {
+                    this.logger.debug(`Batch loader flush after ${flushInterval} seconds interval.`);
+                    await this.flush();
+                }
+            }, flushInterval * 1000);
+        }
     }
 
     async insert(values: any[]) {
@@ -40,17 +71,29 @@ export class BatchLoader {
         }
     }
 
-    flush(): Promise<void> {
-        return new Promise(async (resolve, reject) => {
-            if (this.buf.length > 0) {
-               try {
-                    await this.conn.query(this.sql, [this.buf]);
-                    this.buf = [];
-               } catch (err) {
-                    reject(err);
-               }
+    async flush(): Promise<void> {
+        if (this.buf.length < 0) {
+            return;
+        }
+
+        // Notice: During batch insertion phase as follow, data may still be written to buffer. 
+        // If we clean up the buffer array after the batch insertion is completed, that will result in data loss.
+        // So we can copy it to a temporary memory and clear the buffer so that it can continue to store new data.
+        const records = [[...this.buf]];
+        this.buf = [];
+
+        for (let retries = 0; retries <= this.maxRetries; retries++) {
+            try {
+                await this.conn.query(this.sql, records);
+                return;
+            } catch (err) {
+                if (retries < this.maxRetries) {
+                    this.logger.error(`Failed to batch load ${records.length} records, retries: ${retries + 1}/${this.maxRetries}.`);
+                    continue
+                } else {
+                    this.logger.error(`Failed to batch load ${records.length} records, they will be lost: `, err);
+                }
             }
-            resolve();
-        });
+        }
     }
 }
