d15c9ca5ecf7c572d7c716c4dd23587927cc6a81
Hooopo <Hoooopo@gmail.com>
2022-10-13 17:03:51 +0800

    use left join (#898)

--- backend/lib/trending_repo_generator.rb
+++ backend/lib/trending_repo_generator.rb
@@ -6,10 +6,11 @@ class TrendingRepoGenerator
 
   def generate
     sql = <<~SQL
-      select repo_name, count(distinct actor_login) as count
-      from github_events
-      where type = 'WatchEvent' and created_at >= '#{7.days.ago.to_s(:db)}'
-            and repo_name not in (select repo_name from trending_repos where created_at >= '#{50.days.ago.to_s(:db)}' and repo_name is not null)
+      select github_events.repo_name, count(distinct actor_login) as count
+      from github_events 
+          left join (select repo_name from trending_repos where created_at >= '#{50.days.ago.to_s(:db)}' and repo_name is not null) t on t.repo_name = github_events.repo_name
+      where type = 'WatchEvent' and created_at >= '#{7.days.ago.to_s(:db)}' 
+             and t.repo_name is null
       group by 1
       having count(distinct actor_login) >= 100
       order by 2 desc
