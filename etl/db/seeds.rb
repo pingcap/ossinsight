4023d3aa139244f7fc7483d85a69f8e7f896269f
Hooopo <Hoooopo@gmail.com>
2022-11-28 19:06:12 +0800

    fix tidb ssl seed issue (#985)
    Fix ssl issue

--- etl/db/seeds.rb
+++ etl/db/seeds.rb
@@ -2,5 +2,9 @@
 return if Collection.count > 0
 seed_file = Rails.root.join("db", "seed.sql").to_s
 uri = URI.parse(ENV['DATABASE_URL'])
-cmd = "mysql -u #{uri.user} --port=#{uri.port} --password=#{uri.password} -h #{uri.host} #{uri.path.sub('/', '')} < #{seed_file} "
+if ENV['DB_FORCE_SSL'].present?
+  cmd = "mysql -u #{uri.user} --port=#{uri.port} --password=#{uri.password} -h #{uri.host} #{uri.path.sub('/', '')} --ssl-mode=VERIFY_IDENTITY --ssl-ca=/etc/ssl/cert.pem < #{seed_file} "
+else
+  cmd = "mysql -u #{uri.user} --port=#{uri.port} --password=#{uri.password} -h #{uri.host} #{uri.path.sub('/', '')} < #{seed_file} "
+end
 system(cmd)
