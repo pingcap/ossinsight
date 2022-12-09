7ce39149a00aac762a15d178a9caafead4537f5d
Jagger <634750802@qq.com>
2022-12-09 16:33:33 +0800

    fix(web): user menu state

--- web/src/theme/NavbarItem/LoginItem.tsx
+++ web/src/theme/NavbarItem/LoginItem.tsx
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Avatar, ButtonBase, Menu, MenuItem, styled, useEventCallback } from '@mui/material';
 import { Experimental } from '@site/src/components/Experimental';
 import { useUserInfoContext } from '@site/src/context/user';
+import { useBoolean } from 'ahooks';
 
 export default function LoginItem () {
   const { userInfo, logout } = useUserInfoContext();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [open, setOpen] = useState(false);
-
-  const handleOpen = useEventCallback(() => {
-    setOpen(true);
-  });
+  const [open, { setTrue: handleOpen, setFalse: handleClose }] = useBoolean(false);
 
   const handleGotoSubscriptionsPage = useEventCallback(() => {
     window.open('/subscriptions', '_blank');
@@ -29,6 +26,7 @@ export default function LoginItem () {
         id="basic-menu"
         anchorEl={anchorEl}
         open={open}
+        onClose={handleClose}
         MenuListProps={{
           'aria-labelledby': 'basic-button',
         }}
