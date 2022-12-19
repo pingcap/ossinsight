026351de48f4738b38008d705bca7a7f756d2982
Hooopo Wang <hoooopo@gmail.com>
2022-12-19 08:14:16 +0800

    fetch hackernews items realtime

--- etl/config/environments/development.rb
+++ etl/config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-  config.log_level = :error
+  config.log_level = :debug
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
