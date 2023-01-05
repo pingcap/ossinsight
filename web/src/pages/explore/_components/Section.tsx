77dfcf132e574cefd1b19739f08e5529d56b9e6a
Jagger <634750802@qq.com>
2023-01-05 12:53:02 +0800

    web(explore): adjust section style

--- web/src/pages/explore/_components/Section.tsx
+++ web/src/pages/explore/_components/Section.tsx
@@ -1,4 +1,4 @@
-import { Accordion, AccordionDetails, AccordionSummary, Alert, CircularProgress, styled } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Alert, CircularProgress, Paper, styled } from '@mui/material';
 import React, { ReactNode } from 'react';
 import { CheckCircle, Circle, ExpandMore } from '@mui/icons-material';
 import { isFalsy, isNullish, notFalsy } from '@site/src/utils/value';
@@ -15,56 +15,59 @@ export interface SectionProps {
 }
 
 export default function Section ({ status, title, defaultExpanded, extra, error, errorWithChildren = false, children }: SectionProps) {
+  const alwaysOpen = defaultExpanded === true;
   return (
-    <SectionContainer className={isNullish(error) ? status : 'error'} expanded={defaultExpanded === true ? true : undefined} defaultExpanded={defaultExpanded}>
-      <AccordionSummary
-        expandIcon={isFalsy(defaultExpanded) && <ExpandMore />}
-        disabled={status === 'loading'}
-      >
-        <SectionTitle>
-          {status === 'loading'
-            ? <CircularProgress size={16} />
-            : status === 'success'
-              ? <CheckCircle color="success" fontSize="inherit" />
-              : <Circle color="disabled" fontSize="inherit" />}
-          <SectionTitleContent>
-            {title}
-          </SectionTitleContent>
-          {notFalsy(extra)
-            ? (
-              <>
-                <Spacer />
-                <SectionTitleExtra>
-                  {extra}
-                </SectionTitleExtra>
-              </>
-              )
-            : undefined}
-        </SectionTitle>
-      </AccordionSummary>
-      <AccordionDetails>
-        {errorWithChildren
-          ? isNullish(error)
-            ? children
-            : (
-            <>
-              <Alert severity="error" sx={{ mb: 1 }}>{getErrorMessage(error)}</Alert>
-              {children}
-            </>
-              )
-          : isNullish(error) ? children : <Alert severity="error">{getErrorMessage(error)}</Alert>}
-      </AccordionDetails>
+    <SectionContainer className={isNullish(error) ? status : 'error'} elevation={1}>
+      <SectionAccordion expanded={alwaysOpen ? true : undefined} defaultExpanded={defaultExpanded} elevation={0}>
+        <SectionAccordionSummary
+          alwaysOpen={alwaysOpen}
+          expandIcon={isFalsy(defaultExpanded) && <ExpandMore />}
+          disabled={status === 'loading'}
+        >
+          <SectionTitle>
+            {status === 'loading'
+              ? <CircularProgress size={16} />
+              : status === 'success'
+                ? <CheckCircle color="success" fontSize="inherit" />
+                : <Circle color="disabled" fontSize="inherit" />}
+            <SectionTitleContent>
+              {title}
+            </SectionTitleContent>
+            {notFalsy(extra)
+              ? (
+                <>
+                  <Spacer />
+                  <SectionTitleExtra>
+                    {extra}
+                  </SectionTitleExtra>
+                </>
+                )
+              : undefined}
+          </SectionTitle>
+        </SectionAccordionSummary>
+        <AccordionDetails>
+          {errorWithChildren
+            ? isNullish(error)
+              ? children
+              : (
+                <>
+                  <Alert severity="error" sx={{ mb: 1 }}>{getErrorMessage(error)}</Alert>
+                  {children}
+                </>
+                )
+            : isNullish(error) ? children : <Alert severity="error">{getErrorMessage(error)}</Alert>}
+        </AccordionDetails>
+      </SectionAccordion>
     </SectionContainer>
   );
 }
 
-const SectionContainer = styled(Accordion)`
+const SectionContainer = styled(Paper)`
+  background: linear-gradient(116.45deg, rgba(89, 95, 236, 0.5) 0%, rgba(200, 182, 252, 0.1) 96.73%);
+  padding: 1px;
   border-radius: 6px;
-  border: 1px solid;
-  border-image-source: linear-gradient(116.45deg, rgba(89, 95, 236, 0.5) 0%, rgba(200, 182, 252, 0.1) 96.73%);
-  background: rgb(36, 35, 43);
-  padding: 12px;
   margin-top: 24px;
+
   transform: translateY(20px);
   opacity: 0;
   transition: all .5s ease;
@@ -87,6 +90,17 @@ const SectionContainer = styled(Accordion)`
     transform: initial;
     opacity: 1;
   }
+`;
+
+const SectionAccordion = styled(Accordion)`
+  border: none;
+  background: rgb(36, 35, 43);
+  border-radius: 5px !important;
+  padding: 12px;
+`;
+
+const SectionAccordionSummary = styled(AccordionSummary)<{ alwaysOpen: boolean }>`
+  ${({ alwaysOpen }) => alwaysOpen ? 'cursor: default !important;' : ''}
 `;
 
 const SectionTitle = styled('h2')`
