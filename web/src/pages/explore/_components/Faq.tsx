e474691192549b72f9a23b8a54e3c71d03499179
VitaLoveVitamin <55385323+ChenlingLu@users.noreply.github.com>
2023-01-07 00:13:38 +0800

    web(explorer): update faq (#1090)
    update faq

--- web/src/pages/explore/_components/Faq.tsx
+++ web/src/pages/explore/_components/Faq.tsx
@@ -9,16 +9,18 @@ export default function Faq () {
       <A>You can start with selecting a popular query from the query wall or directly input a short text query statement to the search box. OpenAI will translate your question into Structured Query Language (SQL), and we will use the generated SQL to query in the database and return the results.</A>
       <Q>Why the answer do not turn out to my intended?</Q>
       <A>We use the text completion provided by OpenAI to translate the text into Structured Query Language (SQL), and the final output depends on the accuracy of the generated SQL. If the final query result does not meet your expectation, please shorten your input statement and using more concise and clear short words for AI recognition.</A>
-      <Q>Why is the output show &quot;No Results Found&quot;?</Q>
+      <Q>Why is the output report an error or show 0 results?</Q>
       <A>
-        Unfortunately, we do not have any useful GitHub insights for you. Usually, there are three situations that cause the answer not to be found:
+        Unfortunately, we do not have useful GitHub insights for you. Usually, there are two situations that cause the answer not to be found:
         <ol>
-          <li>AI cannot recognize your text and cannot generate SQLs</li>
-          <li>AI misunderstand your text and generate wrong/bad SQLs</li>
-          <li>Data restrictions. We currently use repositories that have had active events in the past year as our demo source(approximately 1.2 billion rows of data). So if you want to ask a question about &quot;dormant&quot; repositories and developers, we will not be able to pass on more useful information.</li>
+          <li>AI unrecognized/misunderstand your text and generated wrong/bad SQLs, the query mission failed.</li>
+          <li>Data restrictions. All the data we use here on this website sources from&nbsp;
+            <a href='https://github.com/kubernetes/kubernetes/labels?q=size' target='_blank' rel="noreferrer">
+            GH Archive
+          </a>, a non-profit project that records and archives all GitHub events data since 2011. We can not offer you more insights if you are looking for answers about other topics.</li>
         </ol>
         <br />
-        The potential solution for 1,2 is describe your question with short, specific and normal words, then try again.</A>
+        The potential solution is phrase your question which is related GitHub with short, specific words , then try again. And we strongly recommend you use our query templates near the search box to start your exploring.</A>
     </Box>
   );
 }
