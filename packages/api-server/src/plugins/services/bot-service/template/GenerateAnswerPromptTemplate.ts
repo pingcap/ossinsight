961541b3779a10aacb9d169fe92496dee840ea5a
sykp241095 <sykp241095@gmail.com>
2023-01-06 23:39:59 +0800

    Update GenerateAnswerPromptTemplate.ts (#1092)

--- packages/api-server/src/plugins/services/bot-service/template/GenerateAnswerPromptTemplate.ts
+++ packages/api-server/src/plugins/services/bot-service/template/GenerateAnswerPromptTemplate.ts
@@ -12,7 +12,7 @@ export class GenerateAnswerPromptTemplate implements PromptTemplate {
 
   stringify(question: string): string {
     return `# MySQL SQL
-Table github_events, columns = [id, type, created_at, repo_id, repo_name, actor_id, actor_login, language, additions, deletions, action, number, org_login, org_id, state, closed_at, comments, pr_merged_at, pr_merged, pr_changed_files, pr_review_comments, pr_or_issue_id, push_size, push_distinct_size, creator_user_login, creator_user_id, pr_or_issue_created_at]
+Table github_events, columns = [id, type, created_at, repo_id, repo_name, actor_id, actor_login, additions, deletions, action, number, org_login, org_id, state, closed_at, comments, pr_merged_at, pr_merged, pr_changed_files, pr_review_comments, pr_or_issue_id, push_size, push_distinct_size, creator_user_login, creator_user_id, pr_or_issue_created_at]
 Column type, enums = ['PullRequestEvent', 'PushEvent', 'IssueCommentEvent', 'IssuesEvent', 'PullRequestReviewCommentEvent', 'WatchEvent', 'CreateEvent', 'DeleteEvent', 'ForkEvent', 'ReleaseEvent']
 Column number, number is issue number
 Table github_repos, columns = [repo_id, repo_name, owner_id, owner_login, owner_is_org, description, primary_language, license, size, stars, forks, parent_repo_id, is_fork, is_archived, is_deleted, latest_released_at, pushed_at, created_at, updated_at]
@@ -72,7 +72,7 @@ Answer {
 }
 
 ---
-Let's think step by step, use best practice of writing SQL, generate a answer.json file to answer the question: ${question}?
+Let's think step by step, use best practice of writing SQL, use CTE if necessary, if there is no specific repo, scan all repos, generate a answer.json file to answer the question: ${question}?
 ---
 answer.json
 ---
