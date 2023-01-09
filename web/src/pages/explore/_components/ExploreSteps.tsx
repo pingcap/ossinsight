04a025c2690f66196f7df0174e82da2d3682d814
Jagger <634750802@qq.com>
2023-01-09 12:40:43 +0800

    web(explore): optimize explore steps

--- web/src/pages/explore/_components/ExploreSteps.tsx
+++ web/src/pages/explore/_components/ExploreSteps.tsx
@@ -1,6 +1,7 @@
-import { decomposeColor, recomposeColor, Step, stepIconClasses, StepLabel, Stepper, styled } from '@mui/material';
+import { decomposeColor, recomposeColor, Step, stepIconClasses, StepLabel, Stepper, styled, Theme, useMediaQuery } from '@mui/material';
 import React, { useMemo } from 'react';
 import { SxProps } from '@mui/system';
+import { array } from '@site/src/utils/generate';
 
 interface ExploreStepsProps {
   startColor?: string;
@@ -10,22 +11,36 @@ interface ExploreStepsProps {
 }
 
 export default function ExploreSteps ({ startColor = '#794BC5', stopColor = '#3D44FF', steps, sx }: ExploreStepsProps) {
+  const isSmall = useMediaQuery<Theme>(theme => theme.breakpoints.down('md'));
+
   const colorSequences = useMemo(() => {
     const colorStart = decomposeColor('#794BC5').values as RGBColor;
     const colorStop = decomposeColor('#3D44FF').values as RGBColor;
-    return steps.map((_, i) => mixColor(colorStart, colorStop, i / (steps.length - 1)));
+    return array(steps.length + 1).map((i) => mixColor(colorStart, colorStop, i / steps.length));
   }, [startColor, stopColor, steps.length]);
 
   return (
-    <Stepper sx={sx}>
-      {steps.map((step, i) => (
-        <Step key={step} completed={false} active>
-          <StyledStepLabel color={colorSequences[i]}>
-            {step}
-          </StyledStepLabel>
-        </Step>
-      ))}
-    </Stepper>
+    <>
+      <svg width="0" height="0">
+        <defs>
+          {steps.map((_, i) => (
+            <linearGradient id={`explore-step-gradient-${i}`} key={i} x1="0%" y1="0%" x2="100%" y2="0%">
+              <stop offset="0%" style={{ stopColor: colorSequences[i], stopOpacity: 1 }} />
+              <stop offset="100%" style={{ stopColor: colorSequences[i + 1], stopOpacity: 1 }} />
+            </linearGradient>
+          ))}
+        </defs>
+      </svg>
+      <Stepper sx={sx} orientation={isSmall ? 'vertical' : 'horizontal'}>
+        {steps.map((step, i) => (
+          <Step key={step} completed={false} active>
+            <StyledStepLabel fill={`explore-step-gradient-${i}`}>
+              {step}
+            </StyledStepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </>
   );
 }
 
@@ -43,8 +58,8 @@ function mixColor ([r1, g1, b1]: RGBColor, [r2, g2, b2]: RGBColor, p: number) {
   });
 }
 
-const StyledStepLabel = styled(StepLabel, { shouldForwardProp: propName => propName !== 'color' })`
+const StyledStepLabel = styled(StepLabel, { shouldForwardProp: propName => propName !== 'fill' })<{ fill: string }>`
   .${stepIconClasses.root}.${stepIconClasses.active} {
-    color: ${({ color }) => color};
+    fill: url(#${({ fill }) => fill}); // ${({ color }) => color};
   }
 `;
