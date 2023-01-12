13b3741132df072d4f44d02dc903a89615f9f797
ChenlingLu <55385323+ChenlingLu@users.noreply.github.com>
2023-01-12 17:29:40 +0800

    change the og img

--- web/src/pages/explore/index.tsx
+++ web/src/pages/explore/index.tsx
@@ -8,7 +8,7 @@ export default function Page () {
       title="Data Explorer: Discover insights in GitHub event data with AI-generated SQL"
       description="Simply ask your question in natural language and Data Explore will generate SQL, query the data, and present the results visually."
       keywords="GitHub data, text to SQL, query tool, Data Explorer, GPT-3, AI-generated SQL"
-      image="/img/data-thumbnail.png"
+      image="/img/data-explorer-thumbnail.png"
     >
       <Questions />
     </CustomPage>
