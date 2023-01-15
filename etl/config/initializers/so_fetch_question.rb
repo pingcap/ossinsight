1682d38272b46849be7e8a0e17ec2057340c744f
Hooopo Wang <hoooopo@gmail.com>
2023-01-16 04:46:21 +0800

    question realtime

--- etl/config/initializers/so_fetch_question.rb
+++ etl/config/initializers/so_fetch_question.rb
@@ -3,11 +3,10 @@ require 'open-uri'
 require 'yajl'
 
 class SoFetchQuestion
-  attr_reader :ids, :url
+  attr_reader :url
 
-  def initialize(ids)
-    @ids = Array(ids)
-    @url = "https://api.stackexchange.com/2.3/questions/#{ids.join(";")}?key=#{ENV['SO_KEY']}&site=stackoverflow&access_token=#{ENV['SO_ACCESS_TOKEN2']}&pagesize=100&page=1&filter=withbody"
+  def initialize(url)
+    @url = url
   end
 
   def get_response
@@ -31,10 +30,42 @@ class SoFetchQuestion
     json = get_response
     if json
       json = Yajl::Parser.parse(json)
-      puts json
-      json
+      quota_remaining = json["quota_remaining"]
+      puts quota_remaining
+      items = json["items"]
+      attrs = items.map { |item| json_to_attrs(item) }
+      SoQuestion.upsert_all(attrs)
+      #attrs.each do |x|
+      #  SoQuestion.upsert(x) rescue binding.pry
+      #end
     else
       puts "No response"
     end
+  end
+
+  def json_to_attrs(json)
+    attr = {
+      "id"=>json["question_id"],
+      "title"=>json["title"],
+      "body"=>json["body"],
+      "accepted_answer_id"=>json["accepted_answer_id"],
+      "answer_count"=>json["answer_count"],
+      "comment_count"=>json["comment_count"],
+      "community_owned_date"=>nil,
+      "creation_date"=> Time.at(json["creation_date"]),
+      "favorite_count"=>json["favorite_count"],
+      "last_activity_date"=>Time.at(json["last_activity_date"]),
+      "last_edit_date"=> json['last_edit_date'] && Time.at(json["last_edit_date"]),
+      "last_editor_display_name"=>json.dig("last_editor", "display_name"),
+      "last_editor_user_id"=>json.dig("last_editor", "user_id"),
+      "owner_display_name"=>json.dig("owner", "display_name"),
+      "owner_user_id"=> json.dig("owner", "user_id") || -1,
+      "parent_id"=>nil,
+      "post_type_id"=>1,
+      "score"=>json["score"],
+      "tags"=>json["tags"].join(","),
+      "view_count"=>json["view_count"]
+    }
+    attr
   end
 end
--- etl/config/initializers/so_realtime.rb
+++ etl/config/initializers/so_realtime.rb
@@ -1,21 +1,30 @@
-class SoRealtime
-  attr_reader :interval, :from
+require_relative './so_fetch_question'
 
-  def initialize(interval = 10, from = 73842327)
+class SoRealtime
+  attr_reader :interval, :access_token, :url
+
+  def initialize(access_token, interval = 20)
     @interval = interval
-    @from = from
+    @access_token = access_token
+    @url = "https://api.stackexchange.com/2.3/questions?page=1&pagesize=100&order=asc&sort=activity&site=stackoverflow&filter=!)5esRy)TSO9H7Qhh6n-YJ7jtcNBf&access_token=#{ENV['SO_ACCESS_TOKEN2']}&key=#{ENV['SO_KEY']}"
   end
 
   def run
     loop do 
       begin
-        # go here
+        new_url = url + "&min=" + last_activity_date_in_db.to_s
+        SoFetchQuestion.new(new_url).run
       rescue
         ActiveRecord::Base.connection.reconnect!
         puts $!
+        puts 'error'
       end
       sleep(interval)
     end
+  end
+
+  def last_activity_date_in_db
+    (SoQuestion.maximum(:last_activity_date) || Time.parse("2008-01-01 00:00")).to_i
   end
 
   def get_access_token
