dd59c68366689f8b8fb98d973ba832cc4f49fa28
Jagger <634750802@qq.com>
2023-01-16 12:48:04 +0800

    web: add fallback label for number card

--- web/src/pages/explore/_components/charts/NumberCard.tsx
+++ web/src/pages/explore/_components/charts/NumberCard.tsx
@@ -1,28 +1,48 @@
 import { ChartResult } from '@site/src/api/explorer';
 import { Card, CardContent, Grid, List, ListItem, ListItemText, Typography } from '@mui/material';
-import React from 'react';
-import { isFiniteNumber, isNullish, notFalsy } from '@site/src/utils/value';
+import React, { useMemo } from 'react';
+import { isFiniteNumber, isNonemptyString, nonEmptyArray, notFalsy, notNullish } from '@site/src/utils/value';
 import AnimatedNumber from 'react-awesome-animated-number';
 
-export default function NumberCard ({ chartName, title, label, value, data }: ChartResult & { data: any[] }) {
-  if (data.length === 1) {
-    if (data.length === 1 && isNullish(data[0][label])) {
-      const val = data[0][value];
-      return (
-        <Card>
-          <CardContent sx={{ textAlign: 'center', fontSize: 36 }}>
-            <Typography sx={{ opacity: 0.4 }} fontSize={22} mt={2} mb={0} color="text.secondary" gutterBottom align="center">
-              {data[0][label] ?? title}
-            </Typography>
-            {isFiniteNumber(val)
-              ? (
-                <AnimatedNumber value={data[0][value]} hasComma duration={800} size={36} />
-                )
-              : String(val)}
-          </CardContent>
-        </Card>
-      );
+export default function NumberCard ({ chartName, title, label: propLabel, value, data, columns, fields: propFields }: ChartResult & { data: any[], fields?: Array<{ name: string }> }) {
+  const fields = useMemo(() => {
+    if (nonEmptyArray(columns)) {
+      return columns.map((name: string) => ({ name }));
+    } else if (notNullish(propFields)) {
+      return propFields;
+    } else {
+      if (data.length > 0) {
+        return Object.keys(data[0]).map(name => ({ name }));
+      } else {
+        return [{ name: '' }];
+      }
     }
+  }, [data, columns, propFields]);
+
+  const label = useMemo(() => {
+    if (isNonemptyString(propLabel)) {
+      return propLabel;
+    }
+    return propFields?.find(f => /repo|name|user|login/.test(f.name) && f.name !== value)?.name ?? '';
+  }, [fields, propLabel]);
+
+  if (data.length === 1) {
+    const lab = data[0][label] ?? title;
+    const val = data[0][value];
+    return (
+      <Card>
+        <CardContent sx={{ textAlign: 'center', fontSize: 36 }}>
+          <Typography sx={{ opacity: 0.4 }} fontSize={22} mt={2} mb={0} color="text.secondary" gutterBottom align="center">
+            {lab}
+          </Typography>
+          {isFiniteNumber(val)
+            ? (
+              <AnimatedNumber value={data[0][value]} hasComma duration={800} size={36} />
+              )
+            : String(val)}
+        </CardContent>
+      </Card>
+    );
   }
 
   if (notFalsy(label)) {
@@ -34,7 +54,7 @@ export default function NumberCard ({ chartName, title, label, value, data }: Ch
             <Grid key={index} item xs={12} sm={4} md={3} lg={2}>
               <Card sx={{ p: 2 }}>
                 <Typography variant="subtitle1">
-                  {(item[label as string] as string)}
+                  {(item[label] as string)}
                 </Typography>
                 <Typography variant="body2" color="#7c7c7c">
                   {(item[value as string] as string)}
