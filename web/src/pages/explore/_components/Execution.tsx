c52e70288987657225d774a27248e1d1cc3900c6
Jagger <634750802@qq.com>
2023-02-02 19:21:16 +0800

    remove el

--- web/src/pages/explore/_components/Execution.tsx
+++ web/src/pages/explore/_components/Execution.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import useQuestionManagement, { QuestionLoadingPhase } from '@site/src/pages/explore/_components/useQuestion';
-import PoweredBy from '@site/src/pages/explore/_components/PoweredBy';
 import SqlSection from '@site/src/pages/explore/_components/SqlSection';
 import ResultSection from '@site/src/pages/explore/_components/ResultSection';
 
 export default function Execution ({ search }: { search: string }) {
-  const { phase } = useQuestionManagement();
-
   return (
     <>
       <SqlSection />
       <ResultSection />
-      {phase === QuestionLoadingPhase.READY && <PoweredBy sx={{ mt: 2 }} />}
     </>
   );
 };
