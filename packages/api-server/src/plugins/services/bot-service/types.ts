017868e2cc72f6a1e231e2bc382ef94558ac6698
Mini256 <minianter@foxmail.com>
2023-03-02 14:20:50 +0800

    data-explorer: use openai chat API to answer question (#1379)
    * data-explorer: use openai chat API to answer question
    
    * fix: update lock file

--- packages/api-server/src/plugins/services/bot-service/types.ts
+++ packages/api-server/src/plugins/services/bot-service/types.ts
@@ -3,6 +3,7 @@ import {DateTime} from "luxon";
 export enum AIModel {
   TEXT_DAVINCI_002 = 'text-davinci-002',
   TEXT_DAVINCI_003 = 'text-davinci-003',
+  GPT_3_5_TURBO_0301 = 'gpt-3.5-turbo-0301',
   TEXT_CURIE_001 = 'text-curie-001',
   TEXT_BABBAGE_001 = 'text-babbage-001',
   TEXT_ADA_001 = 'text-ada-001',
