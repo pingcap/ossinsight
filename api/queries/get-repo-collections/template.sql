260b4396a5d12ffbd1fd7b6a0aa4e3ef9417f1a9
Mini256 <minianter@foxmail.com>
2023-03-15 17:03:02 +0800

    refactor: move old-api-server to legacy (#1399)

--- api/queries/get-repo-collections/template.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    c.id, ANY_VALUE(c.name) AS name
-FROM
-    collection_items ci
-JOIN collections c ON ci.collection_id = c.id
-WHERE
-    ci.repo_id = 41986369
-GROUP BY c.id;
--- api/queries/stats-query-records/template.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    id, query_name, digest_text, executed_at, UNIX_TIMESTAMP(executed_at) AS ts
-FROM
-    stats_query_summary
-ORDER BY executed_at DESC
-LIMIT 40;
