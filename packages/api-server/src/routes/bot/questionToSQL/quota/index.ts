e0ebdd0f0d36b23e8468569ccd30328f15e9b8ce
Mini256 <minianter@foxmail.com>
2023-03-25 01:55:58 +0800

    explorer: do not pass conn for findOrCreateUser func (#1412)

--- packages/api-server/src/routes/bot/questionToSQL/quota/index.ts
+++ packages/api-server/src/routes/bot/questionToSQL/quota/index.ts
@@ -16,8 +16,7 @@ const root: FastifyPluginAsyncJsonSchemaToTs = async (app): Promise<void> => {
     const { sub, metadata } = parseAuth0User(req.user as Auth0User);
     const userId = await app.userService.findOrCreateUserByAccount(
       { ...metadata, sub },
-      req.headers.authorization,
-      conn
+      req.headers.authorization
     );
     
     try {
