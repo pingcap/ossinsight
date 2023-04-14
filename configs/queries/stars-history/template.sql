fb91ac295f4118be87049b45dd23868007976db3
Mini256 <minianter@foxmail.com>
2023-04-14 18:12:02 +0800

    fix star history

--- configs/queries/stars-history/template.sql
+++ configs/queries/stars-history/template.sql
@@ -1,16 +1,30 @@
-WITH acc AS (
+WITH stars AS (
     SELECT
-        DATE_FORMAT(created_at, '%Y-%m-01')                                    AS t_month,
-        COUNT(actor_login) OVER (ORDER BY DATE_FORMAT(created_at, '%Y-%m-01')) AS total,
-        ROW_NUMBER() OVER (PARTITION BY actor_login)                           AS row_num
+        created_at,
+        ROW_NUMBER() OVER (PARTITION BY actor_login) AS row_num
     FROM github_events
     WHERE
         type = 'WatchEvent'
         AND repo_id = 41986369
+), stars_per_month AS (
+    SELECT
+        DATE_FORMAT(created_at, '%Y-%m-01') AS t_month,
+        COUNT(*) AS total
+    FROM stars
+    WHERE
+        row_num = 1
+    GROUP BY
+        t_month
+), acc AS (
+    SELECT
+        t_month,
+        SUM(total) OVER (ORDER BY t_month) AS total
+    FROM stars_per_month
 )
-SELECT t_month AS event_month, total
+SELECT
+    t_month AS event_month,
+    total
 FROM acc
 GROUP BY t_month
 ORDER BY t_month
 ;
-
