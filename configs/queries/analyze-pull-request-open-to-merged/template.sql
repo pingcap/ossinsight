7418df25c0f6ef505895e4bf847987147d54dd70
Mini256 <minianter@foxmail.com>
2023-04-14 15:31:56 +0800

    refine pr open-to-merged sql

--- configs/queries/analyze-pull-request-open-to-merged/template.sql
+++ configs/queries/analyze-pull-request-open-to-merged/template.sql
@@ -1,76 +1,76 @@
-with pr_with_merged_at as (
-    select
-        number, date_format(created_at, '%Y-%m-01') as t_month, created_at as merged_at
-    from
+WITH pr_with_merged_at AS (
+    SELECT
+        number, DATE_FORMAT(created_at, '%Y-%m-01') AS t_month, created_at AS merged_at
+    FROM
         github_events ge
-    where
+    WHERE
         type = 'PullRequestEvent'
         -- Considering that some repositoies accept the code of the contributor by closing the PR and push commit directly,
         -- here is not distinguished whether it is the merged event.
         -- See: https://github.com/mongodb/mongo/pulls?q=is%3Apr+is%3Aclosed
-        and action = 'closed'
-        and repo_id = 41986369
-), pr_with_opened_at as (
-    select
-        number, created_at as opened_at
-    from
+        AND action = 'closed'
+        AND repo_id = 41986369
+), pr_with_opened_at AS (
+    SELECT
+        number, created_at AS opened_at
+    FROM
         github_events ge
-    where
+    WHERE
         type = 'PullRequestEvent'
-        and action = 'opened'
+        AND action = 'opened'
         -- Exclude Bots
-        -- and actor_login not like '%bot%'
-        -- and actor_login not in (SELECT login FROM blacklist_users bu)
-        and repo_id = 41986369
-), tdiff as (
-    select
+        -- AND actor_login NOT LIKE '%bot%'
+        -- AND actor_login NOT IN (SELECT login FROM blacklist_users bu)
+        AND repo_id = 41986369
+), tdiff AS (
+    SELECT
         t_month,
-        (UNIX_TIMESTAMP(pwm.merged_at) - UNIX_TIMESTAMP(pwo.opened_at)) as diff
-    from
+        (UNIX_TIMESTAMP(pwm.merged_at) - UNIX_TIMESTAMP(pwo.opened_at)) AS diff
+    FROM
         pr_with_opened_at pwo
-        join pr_with_merged_at pwm on pwo.number = pwm.number and pwm.merged_at > pwo.opened_at
-), tdiff_with_rank as (
-    select
+        JOIN pr_with_merged_at pwm ON pwo.number = pwm.number AND pwm.merged_at > pwo.opened_at
+), tdiff_with_rank AS (
+    SELECT
         tdiff.t_month,
-        diff  / 60 / 60 as diff,
-        ROW_NUMBER() over (partition by tdiff.t_month order by diff) as r,
-        count(*) over (partition by tdiff.t_month) as cnt,
-        first_value(diff / 60 / 60) over (partition by tdiff.t_month order by diff) as p0,
-        first_value(diff / 60 / 60)  over (partition by tdiff.t_month order by diff desc) as p100
-    from tdiff
-), tdiff_p25 as (
-    select
-        t_month, diff as p25
-    from
+        diff  / 60 / 60 AS diff,
+        ROW_NUMBER() OVER (PARTITION BY tdiff.t_month ORDER BY diff) AS r,
+        COUNT(*) OVER (PARTITION BY tdiff.t_month) AS cnt,
+        FIRST_VALUE(diff / 60 / 60) OVER (PARTITION BY tdiff.t_month ORDER BY diff) AS p0,
+        FIRST_VALUE(diff / 60 / 60)  OVER (PARTITION BY tdiff.t_month ORDER BY diff DESC) AS p100
+    FROM tdiff
+), tdiff_p25 AS (
+    SELECT
+        t_month, diff AS p25
+    FROM
         tdiff_with_rank tr
-    where
-        r = round(cnt * 0.25)
-), tdiff_p50 as (
-    select
-        t_month, diff as p50
-    from
+    WHERE
+        r = ROUND(cnt * 0.25)
+), tdiff_p50 AS (
+    SELECT
+        t_month, diff AS p50
+    FROM
         tdiff_with_rank tr
-    where
-        r = round(cnt * 0.5)
-), tdiff_p75 as (
-    select
-        t_month, diff as p75
-    from
+    WHERE
+        r = ROUND(cnt * 0.5)
+), tdiff_p75 AS (
+    SELECT
+        t_month, diff AS p75
+    FROM
         tdiff_with_rank tr
-    where
-        r = round(cnt * 0.75)
+    WHERE
+        r = ROUND(cnt * 0.75)
 )
-select
-    distinct tr.t_month AS event_month,
+SELECT
+    tr.t_month AS event_month,
     p0,
     p25,
     p50,
     p75,
     p100
-from tdiff_with_rank tr
-left join tdiff_p25 p25 on tr.t_month = p25.t_month
-left join tdiff_p50 p50 on tr.t_month = p50.t_month
-left join tdiff_p75 p75 on tr.t_month = p75.t_month
-order by 1
+FROM tdiff_with_rank tr
+LEFT JOIN tdiff_p25 p25 ON tr.t_month = p25.t_month
+LEFT JOIN tdiff_p50 p50 ON tr.t_month = p50.t_month
+LEFT JOIN tdiff_p75 p75 ON tr.t_month = p75.t_month
+WHERE r = 1
+ORDER BY event_month
 ;
-
