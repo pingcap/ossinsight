0c83d9823b73af696efa426b845860d62a1e3e4d
Mini256 <minianter@foxmail.com>
2023-04-14 15:46:08 +0800

    refine star-history sqls

--- configs/queries/analyze-stars-history/template.sql
+++ configs/queries/analyze-stars-history/template.sql
@@ -1,14 +1,17 @@
 SELECT
-    t_month AS event_month, total
+    t_month AS event_month,
+    repo_id,
+    total
 FROM (
     SELECT
-        DATE_FORMAT(created_at, '%Y-%m-01')                                   as t_month,
-        COUNT(actor_login) OVER(ORDER BY DATE_FORMAT(created_at, '%Y-%m-01')) AS total,
-        ROW_NUMBER() OVER(PARTITION BY DATE_FORMAT(created_at, '%Y-%m-01'))   AS row_num
+        DATE_FORMAT(created_at, '%Y-%m-01') AS t_month,
+        repo_id,
+        COUNT(actor_login) OVER(ORDER BY repo_id, DATE_FORMAT(created_at, '%Y-%m-01')) AS total,
+        ROW_NUMBER() OVER(PARTITION BY repo_id, DATE_FORMAT(created_at, '%Y-%m-01')) AS row_num
     FROM github_events
     WHERE
         type = 'WatchEvent'
-        AND repo_id = 41986369
+        AND repo_id = (41986369)
     ORDER BY t_month
 ) acc
 WHERE
