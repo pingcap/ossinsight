ab310ed40c004c5b6fa21ee3ff556a79d6022586
Mini256 <minianter@foxmail.com>
2023-06-12 16:52:34 +0800

    fix testes

--- packages/api-server/__tests__/core/executor/query-executor/TiDBPlaygroundQueryExecutor.ts
+++ packages/api-server/__tests__/core/executor/query-executor/TiDBPlaygroundQueryExecutor.ts
@@ -1,21 +1,21 @@
-import { TiDBPlaygroundQueryExecutor } from '../../../../src/core/executor/query-executor/TiDBPlaygroundQueryExecutor';
-import { getConnectionOptions } from '../../../../src/core/db/new';
+
+import {TiDBPlaygroundQueryExecutor} from "../../../../src";
+import {createTiDBPool} from "../../../../src/utils/db";
 import {bootstrapTestDatabase, getTestDatabase, releaseTestDatabase} from '../../../helpers/db';
+import {testLogger} from "../../../helpers/log";
 
 beforeAll(bootstrapTestDatabase);
 afterAll(releaseTestDatabase);
 
 describe('connection limits', () => {
   test('should be executed', async () => {
-    const options = getConnectionOptions({
-        uri: getTestDatabase().url()
-    });
-    const executor = new TiDBPlaygroundQueryExecutor(options, null, [
+    const tidbPool = createTiDBPool(getTestDatabase().url());
+    const executor = new TiDBPlaygroundQueryExecutor(tidbPool, null, testLogger, [
       'set @abc = 1;',
     ]);
 
     const [res] = await executor.execute<any>('', 'SELECT @abc as abc;');
     expect(res[0].abc).toBe(1);
-    await executor.destroy();
+    await tidbPool.end();
   });
 });
