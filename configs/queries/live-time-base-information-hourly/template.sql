1551a7091fedd523b52b14a3edbb53c54a8d9a91
Mini256 <minianter@foxmail.com>
2023-06-17 19:03:33 +0800

    query: improve live time query

--- configs/queries/live-time-base-information-hourly/template.sql
+++ configs/queries/live-time-base-information-hourly/template.sql
@@ -1,13 +1,14 @@
 SELECT
     COUNT(DISTINCT actor_id) AS developers,
     COUNT(DISTINCT repo_id) AS repos,
-    SUM(CASE WHEN action = 'closed' AND pr_merged = true THEN additions ELSE 0 END) AS additions,
-    SUM(CASE WHEN action = 'closed' AND pr_merged = true THEN deletions ELSE 0 END) AS deletions,
-    COUNT(DISTINCT CASE WHEN action = 'opened' THEN pr_or_issue_id ELSE NULL END) AS opened_prs,
-    COUNT(DISTINCT CASE WHEN action = 'closed' AND pr_merged = false THEN pr_or_issue_id ELSE NULL END) AS closed_prs,
-    COUNT(DISTINCT CASE WHEN action = 'closed' AND pr_merged = true THEN pr_or_issue_id ELSE NULL END) AS merged_prs
+    SUM(IF(action = 'closed' AND pr_merged = true, additions, 0)) AS additions,
+    SUM(IF(action = 'closed' AND pr_merged = true, deletions, 0)) AS deletions,
+    COUNT(DISTINCT IF(action = 'opened', pr_or_issue_id, NULL)) AS opened_prs,
+    COUNT(DISTINCT IF(action = 'closed' AND pr_merged = false, pr_or_issue_id, NULL)) AS closed_prs,
+    COUNT(DISTINCT IF(action = 'closed' AND pr_merged = true, pr_or_issue_id, NULL)) AS merged_prs
 FROM
     github_events ge
 WHERE
     type = 'PullRequestEvent'
     AND created_at > DATE_SUB(NOW(), INTERVAL 24 HOUR)
+;
