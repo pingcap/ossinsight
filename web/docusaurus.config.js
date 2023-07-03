e64af7bedc2721addd80ce56d3a1bd12e54ac5e3
sykp241095 <sykp241095@gmail.com>
2023-07-03 11:55:16 +0800

    Fix wording

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -211,9 +211,9 @@ const config = {
         respectPrefersColorScheme: false,
       },
       announcementBar: {
-        id: 'announcement-20230621',
+        id: 'announcement-20230703',
         content:
-          '📢 Customize your own dashboard and embeddable hand-drawn style widgets in 10 minutes, see <a target="_blank" href="https://ossinsight-lite.vercel.app/" style="font-weight:bold">Demo!</a>',
+          '📢 Own your data and get embeddable hand-drawn style widgets in 10 minutes, see <a target="_blank" href="https://ossinsight-lite.vercel.app/" style="font-weight:bold">Demo!</a>',
         backgroundColor: '#4C33B1',
         textColor: '#ffffff',
         isCloseable: true,
