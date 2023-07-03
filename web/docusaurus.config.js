375d797d6a507fbfa8917b1a03b10d848d7f8ec8
sykp241095 <sykp241095@gmail.com>
2023-07-03 15:20:06 +0800

    Use repo url instead of demo url

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -213,7 +213,7 @@ const config = {
       announcementBar: {
         id: 'announcement-20230703',
         content:
-          '📢 Own your data and get embeddable hand-drawn style widgets in 10 minutes, <a target="_blank" href="https://github.com/pingcap/ossinsight-lite" style="font-weight:bold">Go to build!</a>',
+          '📢 Own your data and get embeddable hand-drawn style widgets in 10 minutes, <a target="_blank" href="https://github.com/pingcap/ossinsight-lite" style="font-weight:bold">Go to build !</a>',
         backgroundColor: '#4C33B1',
         textColor: '#ffffff',
         isCloseable: true,
