0aa5ae184b90ec27688d87b33d166a8d860d6dd1
Mini256 <minianter@foxmail.com>
2023-08-02 20:19:42 +0800

    queries: add prefix for map API

---
+++ configs/queries/analyze-issue-creators-map/template.sql
@@ -0,0 +1,22 @@
+WITH group_by_area AS (
+    SELECT
+        gu.country_code AS country_or_area,
+        COUNT(DISTINCT ge.actor_login) as cnt
+    FROM github_events ge
+    LEFT JOIN github_users gu ON ge.actor_login = gu.login
+    WHERE
+        ge.repo_id IN (41986369)
+        AND ge.type = 'IssuesEvent'
+        AND ge.action = 'opened'
+        AND gu.country_code NOT IN ('', 'N/A', 'UND')
+    GROUP BY country_or_area
+), summary AS (
+    SELECT SUM(cnt) AS total FROM group_by_area
+)
+SELECT
+    country_or_area,
+    cnt AS count,
+    cnt / summary.total AS percentage
+FROM group_by_area, summary
+ORDER BY cnt DESC
+;
