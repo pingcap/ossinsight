319f620d69c370eb4ac2481335953fd2318e90c4
Mini256 <minianter@foxmail.com>
2023-08-08 13:59:28 +0800

    stats: get real client IP

--- packages/api-server/src/plugins/stats/api-request-recorder.ts
+++ packages/api-server/src/plugins/stats/api-request-recorder.ts
@@ -1,6 +1,8 @@
+import {FastifyRequest} from "fastify";
 import fp from "fastify-plugin";
 import {Pool} from "mysql2/promise";
 import pino from "pino";
+import { forwarded } from '@fastify/forwarded';
 import {BatchLoader} from "../../core/db/batch-loader";
 import Logger = pino.Logger;
 
@@ -18,13 +20,13 @@ export default fp(async (app) => {
   app.decorate('apiRequestRecorder', apiRequestRecorder);
   app.addHook('onResponse', function (req, reply, done) {
     // Skip metrics request.
-    if (req.url === '/metrics') {
+    if (req.url === '/metrics' || req.url === '/healthy') {
       done();
       return;
     }
 
     // Save the success request.
-    const clientIP = req.headers['X-Real-IP'] ?? req.headers['x-real-ip'] ?? req.ip;
+    const clientIP = getRealClientIP(req);
     const clientOrigin = req.headers.origin ?? '';
     const { method, query } = req;
     const { path } = req.urlData();
@@ -37,7 +39,7 @@ export default fp(async (app) => {
 
   app.addHook('onError', function (req, reply, error, done) {
     // Save the failed request.
-    const clientIP = req.headers['X-Real-IP'] ?? req.headers['x-real-ip'] ?? req.ip;
+    const clientIP = getRealClientIP(req);
     const clientOrigin = req.headers.origin ?? '';
     const { method, query } = req;
     const { path } = req.urlData();
@@ -59,3 +61,25 @@ export default fp(async (app) => {
     '@ossinsight/tidb'
   ],
 });
+
+/**
+ * @summary Get client IP from request object.
+ * @description Get the real client IP from the request.
+ *
+ * **Note**: By default, ELB appends the client's IP to the end of the `X-Forwarded-For` Header value, separated by commas. However,
+ * when the `forwarded` module parses the `X-Forwarded-For` Header, it is stored in the addresses array in reverse order,so the
+ * element with index 0 is the real client IP.
+ *
+ * **Reference**:
+ * https://docs.aws.amazon.com/elasticloadbalancing/latest/application/x-forwarded-headers.html
+ *
+ * @param req The fastify request object.
+ */
+export function getRealClientIP(req: FastifyRequest): string {
+  if (typeof req.headers['x-real-ip'] === 'string') {
+    return req.headers['x-real-ip'];
+  }
+
+  const addresses = forwarded(req as any);
+  return addresses[0] ?? req.ip;
+}
