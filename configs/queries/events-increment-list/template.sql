7727fa258e93f747e442bdd0cbdc1f7e37523f3e
Mini256 <minianter@foxmail.com>
2023-08-14 17:09:25 +0800

    queries: add analyze recent contributors query (#1584)
    * queries: add analyze recent contributors query
    
    * fix workflow
    
    * fix workflow

--- configs/queries/events-increment-list/template.sql
+++ configs/queries/events-increment-list/template.sql
@@ -1,18 +1,22 @@
-SELECT
-    /*+ MAX_EXECUTION_TIME(15000) */
-    id, type, action, actor_id, actor_login, repo_id, repo_name, number, pr_merged, created_at
-FROM github_events 
-WHERE
-    created_at BETWEEN (
-        UTC_TIMESTAMP - INTERVAL 6 MINUTE - INTERVAL UNIX_TIMESTAMP(UTC_TIMESTAMP - INTERVAL 6 MINUTE) % 5 SECOND
-    ) AND (
-        UTC_TIMESTAMP - INTERVAL 5 MINUTE - INTERVAL UNIX_TIMESTAMP(UTC_TIMESTAMP - INTERVAL 5 MINUTE) % 5 SECOND
-    )
-    AND actor_login NOT LIKE '%[bot]'
-    AND actor_login NOT LIKE '%-bot'
-    AND type IN (
-        'WatchEvent', 'ForkEvent', 'IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CreateEvent', 'ReleaseEvent', 
-        'PullRequestReviewCommentEvent', 'PullRequestReviewEvent', 'IssueCommentEvent'
-    )
+SELECT *
+FROM (
+    SELECT
+        /*+ READ_FROM_STORAGE(TIKV[github_events]), MAX_EXECUTION_TIME(15000) */
+        id, type, action, actor_id, actor_login, repo_id, repo_name, number, pr_merged, created_at
+    FROM github_events
+    WHERE
+        created_at BETWEEN (
+            UTC_TIMESTAMP - INTERVAL 6 MINUTE - INTERVAL UNIX_TIMESTAMP(UTC_TIMESTAMP - INTERVAL 6 MINUTE) % 5 SECOND
+        ) AND (
+            UTC_TIMESTAMP - INTERVAL 5 MINUTE - INTERVAL UNIX_TIMESTAMP(UTC_TIMESTAMP - INTERVAL 5 MINUTE) % 5 SECOND
+        )
+        AND actor_login NOT LIKE '%[bot]'
+        AND actor_login NOT LIKE '%-bot'
+        AND type IN (
+            'WatchEvent', 'ForkEvent', 'IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CreateEvent', 'ReleaseEvent',
+            'PullRequestReviewCommentEvent', 'PullRequestReviewEvent', 'IssueCommentEvent'
+        )
+    LIMIT 50
+) sub
 ORDER BY created_at DESC
-LIMIT 50;
+;
