f1fb66186305d80b2440dcfb6b47044ba987f670
Jagger <634750802@qq.com>
2023-09-12 16:12:22 +0800

    refine: remove cte

--- configs/queries/analyze-repo-top-contributors/template.sql
+++ configs/queries/analyze-repo-top-contributors/template.sql
@@ -1,23 +1,18 @@
-WITH contributions AS (
-    SELECT
-        actor_login, COUNT(*) AS events
-    FROM
-        github_events ge
-    WHERE
-        repo_id = 41986369
-        AND (
-            (type = 'PullRequestEvent' AND action = 'opened') OR
-            (type = 'IssuesEvent' AND action = 'opened') OR
-            (type = 'IssueCommentEvent' AND action = 'created') OR
-            (type = 'PullRequestReviewEvent' AND action = 'created') OR
-            (type = 'PullRequestReviewCommentEvent' AND action = 'created') OR
-            (type = 'PushEvent' AND action = '')
-        )
-        AND actor_login NOT LIKE '%bot' AND actor_login NOT LIKE '%[bot]' AND actor_login NOT IN (SELECT login FROM blacklist_users bu)
-    GROUP BY actor_login
-)
-SELECT actor_login, c.events AS events
-FROM contributions c
+SELECT actor_login,
+       COUNT(*) AS events
+FROM github_events ge
+WHERE repo_id = 41986369
+  AND (
+        (type = 'PullRequestEvent' AND action = 'opened') OR
+        (type = 'IssuesEvent' AND action = 'opened') OR
+        (type = 'IssueCommentEvent' AND action = 'created') OR
+        (type = 'PullRequestReviewEvent' AND action = 'created') OR
+        (type = 'PullRequestReviewCommentEvent' AND action = 'created') OR
+        (type = 'PushEvent' AND action = '')
+    )
+  AND actor_login NOT LIKE '%bot'
+  AND actor_login NOT LIKE '%[bot]'
+  AND actor_login NOT IN (SELECT login FROM blacklist_users bu)
 GROUP BY actor_login
-ORDER BY c.events DESC
+ORDER BY events DESC
 LIMIT 9999999999
