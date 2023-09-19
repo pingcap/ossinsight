46f675b6f0e06894cc791e9ac6bce57c25d23da2
Mini256 <minianter@foxmail.com>
2023-09-19 13:48:56 +0800

    queries: using org formatted column in repo analyze

--- packages/api-server/src/core/runner/query/QueryLiquidParser.ts
+++ packages/api-server/src/core/runner/query/QueryLiquidParser.ts
@@ -1,6 +1,6 @@
-import {QuerySchema} from "@ossinsight/types";
+import {Params, QuerySchema} from "@ossinsight/types";
 import {Liquid} from 'liquidjs';
-import {ParamTypes} from "./QueryParser";
+import {ParamItemTypes, ParamTypes} from "./QueryParser";
 
 export class QueryLiquidParser {
 
@@ -12,56 +12,66 @@ export class QueryLiquidParser {
         let context: Record<string, any> = {};
         for (const param of queryConfig.params) {
             const value = values[param.name] ?? param.default;
-            const { type = ParamTypes.STRING } = param;
-            switch (type) {
-                case ParamTypes.ARRAY:
-                    context[param.name] = Array.isArray(value) ? value : [value];
-                    break;
-                case ParamTypes.BOOLEAN:
-                    if (value === 'true') {
-                        context[param.name] = true;
-                    } else if (value === 'false') {
-                        context[param.name] = false;
-                    } else if (typeof value === 'boolean') {
-                        context[param.name] = value;
-                    } else {
-                        throw new Error(`The parameter <${param.name}> is not a boolean.`);
-                    }
-                    break;
-                case ParamTypes.NUMBER:
-                    const num = Number(value);
-                    if (value === null || Number.isNaN(num)) {
-                        throw new Error(`The parameter <${param.name}> is not a number.`);
-                    }
-                    context[param.name] = num;
-                    break;
-                case ParamTypes.INTEGER:
-                    const int = Number(value);
-                    if (value === null || Number.isNaN(int) || !Number.isInteger(int)) {
-                        throw new Error(`The parameter <${param.name}> is not an integer.`);
-                    }
-                    context[param.name] = int;
-                    break;
-                case ParamTypes.STRING:
-                    if (param.pattern) {
-                        const regex = new RegExp(param.pattern);
-                        if (!regex.test(value)) {
-                            throw new Error(`The parameter <${param.name}> does not match the pattern "${param.pattern}".`);
-                        }
-                    }
-                    context[param.name] = value;
-                    break;
-                default:
-                    throw new Error(`The parameter <${param.name}> has an unknown type.`);
+
+            if (param.type === ParamTypes.ARRAY) {
+                const values = Array.isArray(value) ? value : [value];
+                context[param.name] = values.map((itemValue: any) => {
+                    return this.verifyParamValue(param, itemValue);
+                });
+            } else {
+                context[param.name] = this.verifyParamValue(param, value);
             }
 
-            if (Array.isArray(param.enums) && !param.enums.includes(context[param.name])) {
-                throw new Error(`The parameter <${param.name}> is not in the enums.`);
-            }
         }
         return await this.engine.parseAndRender(templateSQL, context, values);
     }
 
+    verifyParamValue(param: Params, value: any) {
+        const type = (param.type === ParamTypes.ARRAY ? param.itemType : param.type) || ParamTypes.STRING;
+        let processedValue = value;
+        switch (type) {
+            case ParamItemTypes.BOOLEAN:
+                if (value === 'true') {
+                    processedValue = true;
+                } else if (value === 'false') {
+                    processedValue = false;
+                } else if (typeof value === 'boolean') {
+                    processedValue = value;
+                } else {
+                    throw new Error(`The parameter <${param.name}> is not a boolean.`);
+                }
+                break;
+            case ParamItemTypes.NUMBER:
+                const num = Number(value);
+                if (value === null || Number.isNaN(num)) {
+                    throw new Error(`The parameter <${param.name}> is not a number.`);
+                }
+                processedValue = num;
+                break;
+            case ParamItemTypes.INTEGER:
+                const int = Number(value);
+                if (value === null || Number.isNaN(int) || !Number.isInteger(int)) {
+                    throw new Error(`The parameter <${param.name}> is not an integer.`);
+                }
+                processedValue = int;
+                break;
+            case ParamItemTypes.STRING:
+                if (param.pattern) {
+                    const regex = new RegExp(param.pattern);
+                    if (!regex.test(value)) {
+                        throw new Error(`The parameter <${param.name}> does not match the pattern "${param.pattern}".`);
+                    }
+                }
+                break;
+            default:
+                throw new Error(`The parameter <${param.name}> has an unknown type.`);
+        }
 
+        if (Array.isArray(param.enums) && !param.enums.includes(processedValue)) {
+            throw new Error(`The parameter <${param.name}> is not in the enums.`);
+        }
+
+        return processedValue;
+    }
 
 }
