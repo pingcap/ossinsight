d47695e3bff1d71f0a30c42bab1731042f473e65
Mini256 <minianter@foxmail.com>
2023-09-22 17:19:47 +0800

    pipeline: fix participants pipelines

--- configs/pipelines/sync_org_repo_participants/process.sql
+++ configs/pipelines/sync_org_repo_participants/process.sql
@@ -1,14 +1,17 @@
-INSERT INTO mv_repo_participants (repo_id, user_id, first_engagement_at)
+INSERT INTO mv_repo_participants_new(repo_id, user_login, first_engagement_at, last_engagement_at)
 SELECT
     ge.repo_id,
-    ge.actor_id AS user_id,
-    MIN(ge.created_at) AS new_first_engagement_at
+    ge.actor_login AS user_login,
+    MIN(ge.created_at) AS new_first_engagement_at,
+    MAX(ge.created_at) AS new_last_engagement_at
 FROM github_events ge
 WHERE
     ge.type != 'WatchEvent'
     AND ge.org_id != 0
     AND ge.created_at >= :from
     AND ge.created_at < :to
-GROUP BY repo_id, actor_id
-ON DUPLICATE KEY UPDATE first_engagement_at = LEAST(first_engagement_at, new_first_engagement_at)
+GROUP BY ge.repo_id, ge.actor_login
+ON DUPLICATE KEY UPDATE
+    first_engagement_at = LEAST(first_engagement_at, new_first_engagement_at)
+    AND last_engagement_at = GREATEST(last_engagement_at, new_last_engagement_at)
 ;
