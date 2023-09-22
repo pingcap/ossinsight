cf39ceda6dbfa348f650552f7ce31ccd939485cc
Mini256 <minianter@foxmail.com>
2023-09-22 14:54:14 +0800

    queries: ⚡ accelerating new participant-related quiries

--- configs/queries/orgs/participants/new/total/template.sql
+++ configs/queries/orgs/participants/new/total/template.sql
@@ -8,20 +8,18 @@ WITH repos AS (
         {% endif %}
 ), old_participants AS (
     SELECT
-        actor_login, MIN(created_at) AS first_participant_at
-    FROM github_events ge
+        mrp.user_id,
+        gu.login AS user_login,
+        mrp.first_engagement_at
+    FROM mv_repo_participants mrp
+    JOIN github_users gu ON mrp.user_id = gu.id
     WHERE
-        ge.repo_id IN (SELECT repo_id FROM repos)
-        -- Events considered as participation (Exclude `WatchEvent`, which means star a repo).
-        AND ge.type IN ('IssueCommentEvent',  'DeleteEvent',  'CommitCommentEvent',  'MemberEvent',  'PushEvent',  'PublicEvent',  'ForkEvent',  'ReleaseEvent',  'PullRequestReviewEvent',  'CreateEvent',  'GollumEvent',  'PullRequestEvent',  'IssuesEvent',  'PullRequestReviewCommentEvent')
-        AND ge.action IN ('added', 'published', 'reopened', 'closed', 'created', 'opened', '')
         {% case period %}
-            {% when 'past_7_days' %} AND ge.created_at < (NOW() - INTERVAL 7 DAY)
-            {% when 'past_28_days' %} AND ge.created_at < (NOW() - INTERVAL 28 DAY)
-            {% when 'past_90_days' %} AND ge.created_at < (NOW() - INTERVAL 90 DAY)
-            {% when 'past_12_months' %} AND ge.created_at < (NOW() - INTERVAL 12 MONTH)
+            {% when 'past_7_days' %} mrp.first_engagement_at < (NOW() - INTERVAL 7 DAY)
+            {% when 'past_28_days' %} mrp.first_engagement_at < (NOW() - INTERVAL 28 DAY)
+            {% when 'past_90_days' %} mrp.first_engagement_at < (NOW() - INTERVAL 90 DAY)
+            {% when 'past_12_months' %} mrp.first_engagement_at < (NOW() - INTERVAL 12 MONTH)
         {% endcase %}
-    GROUP BY actor_login
 ), current_period_new_participants AS (
     SELECT
         COUNT(DISTINCT ge.actor_login) AS new_participants
@@ -43,7 +41,7 @@ WITH repos AS (
         AND NOT EXISTS (
             SELECT 1
             FROM old_participants op
-            WHERE ge.actor_login = op.actor_login
+            WHERE ge.actor_login = op.user_login
         )
 ), past_period_new_participants AS (
     SELECT
@@ -67,12 +65,16 @@ WITH repos AS (
             SELECT 1
             FROM old_participants op
             WHERE
-                ge.actor_login = op.actor_login
+                ge.actor_login = op.user_login
                 {% case period %}
-                    {% when 'past_7_days' %} AND op.first_participant_at < (NOW() - INTERVAL 14 DAY)
-                    {% when 'past_28_days' %} AND op.first_participant_at < (NOW() - INTERVAL 56 DAY)
-                    {% when 'past_90_days' %} AND op.first_participant_at < (NOW() - INTERVAL 180 DAY)
-                    {% when 'past_12_months' %} AND op.first_participant_at < (NOW() - INTERVAL 24 MONTH)
+                {% when 'past_7_days' %}
+                AND op.first_engagement_at < (NOW() - INTERVAL 14 DAY)
+                {% when 'past_28_days' %}
+                AND op.first_engagement_at < (NOW() - INTERVAL 56 DAY)
+                {% when 'past_90_days' %}
+                AND op.first_engagement_at < (NOW() - INTERVAL 180 DAY)
+                {% when 'past_12_months' %}
+                AND op.first_engagement_at < (NOW() - INTERVAL 24 MONTH)
                 {% endcase %}
         )
 )
