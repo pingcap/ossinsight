bd3f16b413e28685c058bf8f4f8544c87e8168fe
Mini256 <minianter@foxmail.com>
2023-10-19 16:03:58 +0800

    *: add org new countries/orgs pipelines and queries

---
+++ configs/materialized_views/mv_repo_countries_commenter_role/ddl.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `mv_repo_countries_commenter_role`
+(
+    `repo_id` INT(11),
+    `country_code` INT(11),
+    `first_seen_at` DATE NOT NULL,
+    PRIMARY KEY (`repo_id`, `country_code`),
+    KEY idx_mrc_cr_on_repo_id_first_seen_at(`repo_id`, `first_seen_at`)
+);
