f433f6fb4bc2bc4c4812cea922b14557edc04d32
Mini256 <minianter@foxmail.com>
2023-11-27 23:57:29 +0800

    pipeline: store collection past month visits

--- packages/api-server/__tests__/migrations/gharchive_dev.collections-schema.sql
+++ packages/api-server/__tests__/migrations/gharchive_dev.collections-schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE `collections` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `public` tinyint(1) DEFAULT '1',
+  `past_month_visits` int default 0 not null,
   PRIMARY KEY (`id`) /*T![clustered_index] CLUSTERED */,
   UNIQUE KEY `index_collections_on_name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin AUTO_INCREMENT=70065;
