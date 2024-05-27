e789c162730065e73b15c92584cd0a2b0fd4c66c
Mini256 <minianter@foxmail.com>
2024-05-27 14:31:11 +0800

    feat: add mv events total

--- configs/materialized_views/mv_events_total/ddl.sql
+++ configs/materialized_views/mv_events_total/ddl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `mv_events_total`
 (
     `record_time` DATETIME,
-    `events_increment` BIGINT(11),
-    `events_total` BIGINT(11),
+    `events_increment` BIGINT(20),
+    `events_total` BIGINT(20),
     PRIMARY KEY (`record_time`)
 );
