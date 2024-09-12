12337b8d55c18e7c33502d2ccfbe7136de24e977
sykp241095 <sykp241095@gmail.com>
2024-09-12 21:37:13 +0800

    Update docusaurus.config.js

--- web/docusaurus.config.js
+++ web/docusaurus.config.js
@@ -234,7 +234,7 @@ const config = {
         respectPrefersColorScheme: false,
       },
       announcementBar: {
-        id: 'announcement-20240910',
+        id: 'announcement-20240912',
         content:
           '<a target="_blank" href="https://ossinsight.io/open-source-heroes/?utm_source=ossinsight&utm_medium=referral&utm_campaign=plg_OSScontribution_credit_05" style="font-weight:bold">Claim up to $2000 in TiDB Serverless credits for your open-source contributions! -></a>',
         backgroundColor: '#4C33B1',
